08bd1c94bf13834d5cfc363fc4f0d373e13a6260
John Safranek <jasaf@mac.com>
2011-12-14 09:54:40 -0800

    Added a test of Init to the tests.

--- tests/api.c
+++ tests/api.c
@@ -1,1 +1,13 @@
 /* api.c API unit tests */
+#include <cyassl/ssl.h>
+
+// 0 on success, otherwise fail
+int test_CyaSSL_Init(void)
+{
+    int result = CyaSSL_Init();
+    
+    if (result) printf("test_CyaSSL_Init(): failed\n");
+
+    return (result);
+}
+
