c0783e4ec404657f97f173a488b583e3cc477e17
John Safranek <jasaf@mac.com>
2012-04-13 09:39:19 -0700

    added configure option and a stub source file for OCSP

---
+++ src/ocsp.c
@@ -0,0 +1,35 @@
+/* ocsp.c
+ *
+ * Copyright (C) 2006-2012 Sawtooth Consulting Ltd.
+ *
+ * This file is part of CyaSSL.
+ *
+ * CyaSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CyaSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#include <cyassl/error.h>
+#include <cyassl/ctaocrypt/asn.h>
+
+
+#ifdef HAVE_OCSP
+
+void ocsp_stub(void) {}
+
+#endif /* HAVE_OCSP */
+
