8f30850840221ec71a08e2de62da333f77b55939
John Safranek <jasaf@mac.com>
2012-06-05 10:35:30 -0700

    CRL fix label in debug output

--- src/crl.c
+++ src/crl.c
@@ -55,7 +55,7 @@ int InitCRL(CYASSL_CRL* crl, CYASSL_CERT_MANAGER* cm)
 /* Initialze CRL Entry */
 static int InitCRL_Entry(CRL_Entry* crle, DecodedCRL* dcrl)
 {
-    CYASSL_ENTER("FreeCRL_Entry");
+    CYASSL_ENTER("InitCRL_Entry");
 
     XMEMCPY(crle->issuerHash, dcrl->issuerHash, SHA_DIGEST_SIZE);
     XMEMCPY(crle->crlHash, dcrl->crlHash, MD5_DIGEST_SIZE);
