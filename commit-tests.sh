e2ae36f149bdde75c59c1392469e9b1e64daf52c
toddouska <todd@yassl.com>
2012-08-20 16:40:41 -0700

    add multi job support to first precommit make test

--- commit-tests.sh
+++ commit-tests.sh
@@ -5,7 +5,7 @@
 
 # make sure current config is ok
 echo -e "\n\nTesting current config...\n\n"
-make test;
+make -j 8 test;
 RESULT=$?
 [ $RESULT -ne 0 ] && echo -e "\n\nCurrent config make test failed" && exit 1
 
