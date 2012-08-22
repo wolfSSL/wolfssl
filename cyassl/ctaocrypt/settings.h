87762e9012a1d4914b66936f842b905059707b9e
toddouska <todd@yassl.com>
2012-08-22 11:07:40 -0700

    SafeRTOS client test fixes

--- cyassl/ctaocrypt/settings.h
+++ cyassl/ctaocrypt/settings.h
@@ -150,6 +150,8 @@
 #endif
 
 #ifdef CYASSL_LSR
+    #define SIZEOF_LONG_LONG 8
+    #define CYASSL_LOW_MEMORY
     #define NO_WRITEV
     #define NO_SHA512
     #define NO_DH
@@ -160,6 +162,7 @@
     #define NO_RABBIT
     #ifndef NO_FILESYSTEM
         #define LSR_FS
+        #include "inc/hw_types.h"
         #include "fs.h"
     #endif
     #define CYASSL_LWIP
@@ -179,6 +182,12 @@
     #define XMALLOC(s, h, type)  pvPortMalloc((s))
     #define XFREE(p, h, type)    vPortFree((p)) 
     #define XREALLOC(p, n, h, t) pvPortRealloc((p), (n))
+#endif
+
+#ifdef CYASSL_LOW_MEMORY
+    #define RSA_LOW_MEM
+    #define CYASSL_SMALL_STACK
+    #define TFM_TIMING_RESISTANT
 #endif
 
 #ifdef MICRIUM
