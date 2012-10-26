994fc1f8e369983dfdf0dd097cd5df6d5553beec
Brian Aker <brian@tangent.org>
2012-10-26 02:06:06 -0400

    This add the generic structure required to have pkgconfig work.
    I also fixed autogen.sh to do some magic on warnings/errors based on whether code comes from github or not.

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,11 @@
 # Create configure and makefile stuff...
 #
 
-autoreconf -ivf -Wall
+if test -d .git; then
+  WARNINGS="all,error"
+else
+  WARNINGS="all"
+fi
+
+autoreconf --install --force --verbose
 ln -s -f ../../pre-commit.sh .git/hooks/pre-commit
