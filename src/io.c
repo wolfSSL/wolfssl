254d53bb18ce27db47b4ef1d5076de9133b9505b
Chris Conlon <chris@yassl.com>
2012-12-27 10:06:29 -0700

    add CYASSL_STM32F2 define, LwIP errno.h

--- src/io.c
+++ src/io.c
@@ -45,6 +45,7 @@
         /* lwIP needs to be configured to use sockets API in this mode */
         /* LWIP_SOCKET 1 in lwip/opt.h or in build */
         #include "lwip/sockets.h"
+		#include <errno.h>
         #ifndef LWIP_PROVIDE_ERRNO
             #define LWIP_PROVIDE_ERRNO 1
         #endif
