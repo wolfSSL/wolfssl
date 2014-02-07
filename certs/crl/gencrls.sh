9a275fbe8b305624bc4d87e4818189dfd93773d9
John Safranek <john@wolfssl.com>
2014-02-07 12:17:01 -0800

    updated CRLs for a year

--- certs/crl/gencrls.sh
+++ certs/crl/gencrls.sh
@@ -5,7 +5,7 @@
 
 
 # caCrl
-openssl ca -gencrl -crldays 180 -out crl.pem -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
+openssl ca -gencrl -crldays 365 -out crl.pem -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
 
 # metadata
 openssl crl -in crl.pem -text > tmp
@@ -17,7 +17,7 @@ cp crl.pem ~/cyassl/certs/crl/crl.pem
 openssl ca -revoke ~/cyassl/certs/server-cert.pem -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
 
 # caCrl server revoked generation
-openssl ca -gencrl -crldays 180 -out crl.revoked -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
+openssl ca -gencrl -crldays 365 -out crl.revoked -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
 
 # metadata
 openssl crl -in crl.revoked -text > tmp
@@ -29,7 +29,7 @@ cp crl.revoked ~/cyassl/certs/crl/crl.revoked
 cp blank.index.txt demoCA/index.txt
 
 # cliCrl
-openssl ca -gencrl -crldays 180 -out cliCrl.pem -keyfile ~/cyassl/certs/client-key.pem -cert ~/cyassl/certs/client-cert.pem
+openssl ca -gencrl -crldays 365 -out cliCrl.pem -keyfile ~/cyassl/certs/client-key.pem -cert ~/cyassl/certs/client-cert.pem
 
 # metadata
 openssl crl -in cliCrl.pem -text > tmp
@@ -38,7 +38,7 @@ mv tmp cliCrl.pem
 cp cliCrl.pem ~/cyassl/certs/crl/cliCrl.pem
 
 # eccCliCRL
-openssl ca -gencrl -crldays 180 -out eccCliCRL.pem -keyfile ~/cyassl/certs/ecc-client-key.pem -cert ~/cyassl/certs/client-ecc-cert.pem
+openssl ca -gencrl -crldays 365 -out eccCliCRL.pem -keyfile ~/cyassl/certs/ecc-client-key.pem -cert ~/cyassl/certs/client-ecc-cert.pem
 
 # metadata
 openssl crl -in eccCliCRL.pem -text > tmp
@@ -47,7 +47,7 @@ mv tmp eccCliCRL.pem
 cp eccCliCRL.pem ~/cyassl/certs/crl/eccCliCRL.pem
 
 # eccSrvCRL
-openssl ca -gencrl -crldays 180 -out eccSrvCRL.pem -keyfile ~/cyassl/certs/ecc-key.pem -cert ~/cyassl/certs/server-ecc.pem
+openssl ca -gencrl -crldays 365 -out eccSrvCRL.pem -keyfile ~/cyassl/certs/ecc-key.pem -cert ~/cyassl/certs/server-ecc.pem
 
 # metadata
 openssl crl -in eccSrvCRL.pem -text > tmp
