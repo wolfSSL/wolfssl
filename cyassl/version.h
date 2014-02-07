594feec68b9d87a40942001ffbbf22b09bf731d5
John Safranek <john@wolfssl.com>
2014-02-07 12:28:41 -0800

    v2.9.0 release

--- cyassl/version.h
+++ cyassl/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBCYASSL_VERSION_STRING "2.8.6"
-#define LIBCYASSL_VERSION_HEX 0x02008006
+#define LIBCYASSL_VERSION_STRING "2.9.0"
+#define LIBCYASSL_VERSION_HEX 0x02009000
 
 #ifdef __cplusplus
 }
