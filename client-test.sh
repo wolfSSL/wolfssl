ef8458f7541de5f39794fded37560f00ae7c881b
toddouska <todd@wolfssl.com>
2014-03-20 10:37:53 -0700

    return right away after valid ping response, use TLSv1.2 for google

--- client-test.sh
+++ client-test.sh
@@ -5,12 +5,12 @@
 [ ! -x ./examples/client/client ] && echo -e "\n\nClient doesn't exist" && exit 1
 
 # is our desired server there?
-ping -c 3 www.google.com
+ping -c 3 -o www.google.com
 RESULT=$?
 [ $RESULT -ne 0 ] && echo -e "\n\nCouldn't find server, skipping" && exit 0
 
 # client test against the server
-./examples/client/client -h www.google.com -p 443 -g -d -v 1
+./examples/client/client -h www.google.com -p 443 -g -d
 RESULT=$?
 [ $RESULT -ne 0 ] && echo -e "\n\nClient connection failed" && exit 1
 
