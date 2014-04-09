06faa470012e66edf0f8aed8df71f734929d2e73
toddouska <todd@wolfssl.com>
2014-04-09 09:51:11 -0700

    version bump

--- cyassl/version.h
+++ cyassl/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBCYASSL_VERSION_STRING "2.9.3"
-#define LIBCYASSL_VERSION_HEX 0x02009003
+#define LIBCYASSL_VERSION_STRING "2.9.4"
+#define LIBCYASSL_VERSION_HEX 0x02009004
 
 #ifdef __cplusplus
 }
