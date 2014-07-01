1122f2a3993c4e8465c82d08a64d5fc7482e42d8
toddouska <todd@wolfssl.com>
2014-07-01 11:58:33 -0700

    master merge resolve

--- gencertbuf.pl
+++ gencertbuf.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
 # gencertbuf.pl
-# version 1.0
-# Updated 01/28/2013
+# version 1.1
+# Updated 07/01/2014
 #
-# Copyright (C) 2006-2013 wolfSSL
+# Copyright (C) 2006-2014 wolfSSL Inc.
 #
 
 use strict;
@@ -34,7 +34,10 @@ my @fileList_2048 = (
         [ "./certs/client-cert.der", "client_cert_der_2048" ],
         [ "./certs/dh2048.der", "dh_key_der_2048" ],
         [ "./certs/dsa2048.der", "dsa_key_der_2048" ],
-        [ "./certs/rsa2048.der", "rsa_key_der_2048" ]
+        [ "./certs/rsa2048.der", "rsa_key_der_2048" ],
+        [ "./certs/ca-cert.der", "ca_cert_der_2048" ],
+        [ "./certs/server-key.der", "server_key_der_2048" ],
+        [ "./certs/server-cert.der", "server_cert_der_2048" ]
         );
 
 # ----------------------------------------------------------------------------
@@ -51,22 +54,32 @@ print OUT_FILE "#define CYASSL_CERTS_TEST_H\n\n";
 
 # convert and print 1024-bit cert/keys
 print OUT_FILE "#ifdef USE_CERT_BUFFERS_1024\n\n";
-for(my $i = 0; $i < $num_1024; $i++) {
-    print OUT_FILE "/* $fileList_1024[$i][0], 1024-bit */\n";
-    print OUT_FILE "const unsigned char $fileList_1024[$i][1]\[] =\n";
+for (my $i = 0; $i < $num_1024; $i++) {
+
+    my $fname = $fileList_1024[$i][0];
+    my $sname = $fileList_1024[$i][1];
+
+    print OUT_FILE "/* $fname, 1024-bit */\n";
+    print OUT_FILE "const unsigned char $sname\[] =\n";
     print OUT_FILE "{\n";
-    file_to_hex($fileList_1024[$i][0]);
-    print OUT_FILE "};\n\n";
+    file_to_hex($fname);
+    print OUT_FILE "};\n";
+    print OUT_FILE "const int sizeof_$sname = sizeof($sname);\n\n";
 }
 
 # convert and print 2048-bit certs/keys
 print OUT_FILE "#elif defined(USE_CERT_BUFFERS_2048)\n\n";
-for(my $i = 0; $i < $num_2048; $i++) {
-    print OUT_FILE "/* $fileList_2048[$i][0], 2048-bit */\n";
-    print OUT_FILE "const unsigned char $fileList_2048[$i][1]\[] =\n";
+for (my $i = 0; $i < $num_2048; $i++) {
+
+    my $fname = $fileList_2048[$i][0];
+    my $sname = $fileList_2048[$i][1];
+
+    print OUT_FILE "/* $fname, 2048-bit */\n";
+    print OUT_FILE "const unsigned char $sname\[] =\n";
     print OUT_FILE "{\n";
-    file_to_hex($fileList_2048[$i][0]);
-    print OUT_FILE "};\n\n";
+    file_to_hex($fname);
+    print OUT_FILE "};\n";
+    print OUT_FILE "const int sizeof_$sname = sizeof($sname);\n\n";
 }
 
 print OUT_FILE "#endif /* USE_CERT_BUFFERS_1024 */\n\n";
@@ -108,3 +121,4 @@ sub file_to_hex {
 
     close($fp); 
 }
+
