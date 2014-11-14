de388bf37f22a5458dcc26d6393b2fd20d778808
toddouska <todd@wolfssl.com>
2014-11-14 11:49:14 -0800

    version 3.2.6

--- cyassl/version.h
+++ cyassl/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBCYASSL_VERSION_STRING "3.2.4"
-#define LIBCYASSL_VERSION_HEX 0x03002004
+#define LIBCYASSL_VERSION_STRING "3.2.6"
+#define LIBCYASSL_VERSION_HEX 0x03002006
 
 #ifdef __cplusplus
 }
