15bffb2a1e314d0b876036d41f908323504e80ac
toddouska <todd@wolfssl.com>
2014-12-05 09:37:19 -0800

    prepare 3.3.0 release

--- cyassl/version.h
+++ cyassl/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBCYASSL_VERSION_STRING "3.2.6"
-#define LIBCYASSL_VERSION_HEX 0x03002006
+#define LIBCYASSL_VERSION_STRING "3.3.0"
+#define LIBCYASSL_VERSION_HEX 0x03003000
 
 #ifdef __cplusplus
 }
