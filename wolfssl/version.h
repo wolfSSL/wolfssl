f06c08718c0e2cca0cfb12082d09b0a4c50669c7
John Safranek <john@wolfssl.com>
2015-10-05 10:14:07 -0700

    bump version for sniffer release

--- wolfssl/version.h
+++ wolfssl/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFSSL_VERSION_STRING "3.6.8"
-#define LIBWOLFSSL_VERSION_HEX 0x03006008
+#define LIBWOLFSSL_VERSION_STRING "3.6.9"
+#define LIBWOLFSSL_VERSION_HEX 0x03006009
 
 #ifdef __cplusplus
 }
