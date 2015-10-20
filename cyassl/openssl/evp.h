f64f326ab3deb1d71151196ff88a40f809694076
Jacob Barthelmeh <jacob@wolfssl.com>
2015-10-20 15:25:00 -0600

    work on cyassl -> wolfssl compatibility

--- cyassl/openssl/evp.h
+++ cyassl/openssl/evp.h
@@ -24,4 +24,17 @@
  *
  */
 
+#ifndef CYASSL_OPENSSL_EVP
+#define CYASSL_OPENSSL_EVP
+
+#define CyaSSL_StoreExternalIV       wolfSSL_StoreExternalIV
+#define CyaSSL_SetInternalIV         wolfSSL_SetInternalIV
+#define CYASSL_EVP_MD                WOLFSSL_EVP_MD
+#define CyaSSL_EVP_X_STATE           wolfSSL_EVP_X_STATE
+#define CyaSSL_EVP_X_STATE_LEN       wolfSSL_EVP_X_STATE_LEN
+#define CyaSSL_3des_iv               wolfSSL_3des_iv
+#define CyaSSL_aes_ctr_iv            wolfSSL_aes_ctr_iv
+
 #include <wolfssl/openssl/evp.h>
+#endif
+
