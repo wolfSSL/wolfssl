4b836f8476ce4874d07939b587d5647dbadd45a5
John Safranek <john@wolfssl.com>
2015-12-23 12:20:53 -0800

    added note to client and server regarding port 0

--- examples/client/client.c
+++ examples/client/client.c
@@ -52,6 +52,10 @@
 
 #include "examples/client/client.h"
 
+/* Note on using port 0: the client and server standalone examples don't
+ * utilize the port 0 port sharing; that is used by the testsuite which uses
+ * this code and sets up the correct port numbers when the internal thread,
+ * using the server code, uses port 0. */
 
 #ifdef WOLFSSL_CALLBACKS
     int handShakeCB(HandShakeInfo*);
