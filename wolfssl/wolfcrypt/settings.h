ba4ecd5c803cf39d109e3b569ff36dc1224b506f
Michał Lower <keton22@gmail.com>
2016-09-24 14:11:58 +0200

    Remove ECC_TIMING_RESISTANT that caused high memopry usage; Added comment with option to endble nRF5x support

--- wolfssl/wolfcrypt/settings.h
+++ wolfssl/wolfcrypt/settings.h
@@ -124,6 +124,9 @@
 /* Uncomment next line if building for VxWorks */
 /* #define WOLFSSL_VXWORKS */
 
+/* Uncomment next line if building for Nordic nRF5x platofrm */
+/* #define WOLFSSL_NRF5x */
+
 /* Uncomment next line to enable deprecated less secure static DH suites */
 /* #define WOLFSSL_STATIC_DH */
 
@@ -412,7 +415,6 @@
 		#define SINGLE_THREADED
 		#define USE_FAST_MATH
 		#define TFM_TIMING_RESISTANT
-		#define ECC_TIMING_RESISTANT
 		#define USE_WOLFSSL_MEMORY
 		#define WOLFSSL_NRF51
 		#define WOLFSSL_USER_IO
