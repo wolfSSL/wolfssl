6013df08a0177ccdf780943511fbd2b3e2052daf
Takashi Kojo <kojo@wolfssl.com>
2016-10-14 10:58:49 +0900

    update license claim

--- IDE/LwIP-Native/client-nb.c
+++ IDE/LwIP-Native/client-nb.c
@@ -1,6 +1,6 @@
 /* client-nb.c
  *
- * Copyright (C) 2006-2014 wolfSSL Inc.
+ * Copyright (C) 2006-2016 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
@@ -16,8 +16,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
  */
+
 
 #ifdef HAVE_CONFIG_H
     #include <config.h>
