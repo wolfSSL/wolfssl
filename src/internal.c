060ff5e5ef5a2d4ecde828205db3382fdf3f8a34
Chris Conlon <chris@wolfssl.com>
2016-12-19 11:53:14 -0700

    address fortify critical issues

--- src/internal.c
+++ src/internal.c
@@ -10527,7 +10527,9 @@ int SendCertificate(WOLFSSL* ssl)
 
             sendSz = BuildMessage(ssl, output, sendSz, input, inputSz,
                                   handshake, 1, 0);
-            XFREE(input, ssl->heap, DYNAMIC_TYPE_TMP_BUFFER);
+
+            if (inputSz > 0)
+                XFREE(input, ssl->heap, DYNAMIC_TYPE_TMP_BUFFER);
 
             if (sendSz < 0)
                 return sendSz;
