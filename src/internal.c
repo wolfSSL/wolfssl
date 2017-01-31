fbd7f7972bd52ee8e78dd6ee71b02da888943fc8
John Safranek <john@wolfssl.com>
2017-01-31 15:11:34 -0800

    Multicast 1. Used quotes rather than angle-brackets when including    user_settings.h. 2. Used angle-brackets rather than quotes when including    the ThreadX and NetX headers. 3. Added a define flag to include types.h with NetX or ThreadX. 4. Added a void typecast to hush a warning about an unused variable    in the I/O callbacks for NetX. 5. Clean up static analysis warning in the peer sequence    number selection for DTLS.

--- src/internal.c
+++ src/internal.c
@@ -9739,10 +9739,8 @@ static INLINE int DtlsUpdateWindow(WOLFSSL* ssl)
     cur_hi = ssl->keys.curSeq_hi;
     cur_lo = ssl->keys.curSeq_lo;
 
-    if (!ssl->options.haveMcast)
-        peerSeq = ssl->keys.peerSeq;
-    else {
 #ifdef WOLFSSL_MULTICAST
+    if (ssl->options.haveMcast) {
         WOLFSSL_DTLS_PEERSEQ* p;
         int i;
 
@@ -9779,8 +9777,8 @@ static INLINE int DtlsUpdateWindow(WOLFSSL* ssl)
                                                    ssl->ctx->mcastSecondSeq,
                                                    ssl->ctx->mcastMaxSeq);
         }
-#endif
     }
+#endif
 
     if (ssl->keys.curEpoch == peerSeq->nextEpoch) {
         next_hi = &peerSeq->nextSeq_hi;
