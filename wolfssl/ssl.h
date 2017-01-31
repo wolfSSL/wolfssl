2086394a35e8c4373e237bb4fa16c358f387fa3f
Jacob Barthelmeh <jacob@wolfssl.com>
2017-01-31 14:40:21 -0700

    compatibility of get cipher list function and update cmake files

--- wolfssl/ssl.h
+++ wolfssl/ssl.h
@@ -388,6 +388,7 @@ WOLFSSL_API int  wolfSSL_set_fd (WOLFSSL*, int);
 WOLFSSL_API int  wolfSSL_set_write_fd (WOLFSSL*, int);
 WOLFSSL_API int  wolfSSL_set_read_fd (WOLFSSL*, int);
 WOLFSSL_API char* wolfSSL_get_cipher_list(int priority);
+WOLFSSL_API char* wolfSSL_get_cipher_list_ex(WOLFSSL* ssl, int priority);
 WOLFSSL_API int  wolfSSL_get_ciphers(char*, int);
 WOLFSSL_API const char* wolfSSL_get_cipher_name(WOLFSSL* ssl);
 WOLFSSL_API const char* wolfSSL_get_shared_ciphers(WOLFSSL* ssl, char* buf,
