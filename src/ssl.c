2cc705707174b69dc326ff053942e407e5128533
Takashi Kojo <kojo@wolfsssl.com>
2017-04-07 11:53:24 +0900

    Rollback

--- src/ssl.c
+++ src/ssl.c
@@ -6999,7 +6999,6 @@ WOLFSSL_EVP_PKEY* wolfSSL_d2i_PUBKEY(WOLFSSL_EVP_PKEY** out, unsigned char** in,
                 pkey->type = EVP_PKEY_RSA;
                 if (out != NULL) {
                     *out = pkey;
-                    pkey->rsa = (WOLFSSL_RSA*)pkey->pkey.ptr;
                 }
 
                 pkey->ownRsa = 1;
