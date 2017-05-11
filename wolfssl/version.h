7d6597fe554ee5e9b0e042f484b6a7c22cfb4c80
Chris Conlon <chris@wolfssl.com>
2017-05-11 10:01:04 -0600

    wolfSSL 3.11.1 release, TLS 1.3 BETA

--- wolfssl/version.h
+++ wolfssl/version.h
@@ -28,8 +28,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFSSL_VERSION_STRING "3.11.0"
-#define LIBWOLFSSL_VERSION_HEX 0x03011000
+#define LIBWOLFSSL_VERSION_STRING "3.11.1"
+#define LIBWOLFSSL_VERSION_HEX 0x03011001
 
 #ifdef __cplusplus
 }
