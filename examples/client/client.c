0e34f35c08afcb8ccfb00ae7c63ea9d61803a982
David Garske <david@wolfssl.com>
2017-10-26 08:06:08 -0700

    Increase the static memory pool in client to better support ECC or session certs.

--- examples/client/client.c
+++ examples/client/client.c
@@ -889,7 +889,7 @@ THREAD_RETURN WOLFSSL_THREAD client_test(void* args)
     #if (defined(HAVE_ECC) && !defined(ALT_ECC_SIZE)) \
         || defined(SESSION_CERTS)
         /* big enough to handle most cases including session certs */
-        byte memory[204000];
+        byte memory[320000];
     #else
         byte memory[80000];
     #endif
