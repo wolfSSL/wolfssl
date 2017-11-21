2308b0769a8cff4c796706d8a13199bfa283abc1
abrahamsonn <alex@wolfssl.com>
2017-11-21 14:15:47 -0700

    Changed "ifdef _WIN32" to "ifdef USE_WINDOWS_API"

--- wolfssl/wolfio.h
+++ wolfssl/wolfio.h
@@ -406,14 +406,14 @@ WOLFSSL_API void wolfSSL_SetIOWriteFlags(WOLFSSL* ssl, int flags);
 
 #ifndef XINET_NTOP
     #define XINET_NTOP(a,b,c,d) inet_ntop((a),(b),(c),(d))
-    #ifdef _WIN32 /* Windows-friendly definition */
+    #ifdef USE_WINDOWS_API /* Windows-friendly definition */
         #undef  XINET_NTOP
         #define XINET_NTOP(a,b,c,d) InetNtop((a),(b),(c),(d))
     #endif
 #endif
 #ifndef XINET_PTON
     #define XINET_PTON(a,b,c)   inet_pton((a),(b),(c))
-    #ifdef _WIN32 /* Windows-friendly definition */
+    #ifdef USE_WINDOWS_API /* Windows-friendly definition */
         #undef  XINET_PTON
         #define XINET_PTON(a,b,c)   InetPton((a),(b),(c))
     #endif
