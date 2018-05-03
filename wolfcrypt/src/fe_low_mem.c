ecd2e75564b58e1053e4bb4df3703870bb169840
Takashi Kojo <kojo@wolfssl.com>
2018-05-04 07:34:47 +0900

    #ifndef FREESCALE_LTC_ECC with fe_init

--- wolfcrypt/src/fe_low_mem.c
+++ wolfcrypt/src/fe_low_mem.c
@@ -55,6 +55,7 @@ void lm_copy(byte* x, const byte* a)
         x[i] = a[i];
 }
 
+#ifndef FREESCALE_LTC_ECC
 #if ((defined(HAVE_CURVE25519) && !defined(CURVE25519_SMALL)) || \
 	 (defined(HAVE_ED25519) && !defined(ED25519_SMALL))) &&      \
 	!defined(FREESCALE_LTC_ECC)
@@ -63,6 +64,7 @@ void lm_copy(byte* x, const byte* a)
 void fe_init()
 {
 }
+#endif
 #endif
 
 #ifdef CURVE25519_SMALL
