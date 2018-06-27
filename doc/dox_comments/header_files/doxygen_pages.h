f32bd77a1dea964d0cb2280bbaa2698f286020c2
abrahamsonn <alex@wolfssl.com>
2018-06-27 16:29:37 -0600

    Updating appearance
    In the past, Doxygen just listed API for wolfSSL and wolfCrypt in one long list.
    Now there are two separate pages for the wolfSSL and wolfCrypt API references.

---
+++ doc/dox_comments/header_files/doxygen_pages.h
@@ -0,0 +1,52 @@
+/*!
+    \page wolfssl_API wolfSSL API Reference
+    - \ref CertManager
+    - \ref Memory
+    - \ref openSSL
+
+    - \ref CertsKeys
+    - \ref IO
+    - \ref Setup
+    - \ref Debug
+    - \ref TLS
+*/
+/*!
+    \page wolfcrypt_API wolfCrypt API Reference
+    - \ref ASN
+    - \ref Base_Encoding
+    - \ref Compression
+    - \ref Error
+    - \ref Keys
+    - \ref Logging
+    - \ref Math
+    - \ref Random
+    - \ref Signature
+    - \ref wolfCrypt
+
+    - \ref DES
+    - \ref AES
+    - \ref ARC4
+    - \ref BLAKE2
+    - \ref Camellia
+    - \ref ChaCha
+    - \ref ChaCha20Poly1305
+    - \ref Curve25519
+    - \ref DSA
+    - \ref Diffie-Hellman
+    - \ref ECC
+    - \ref ED25519
+    - \ref HC128
+    - \ref HMAC
+    - \ref IDEA
+    - \ref MD2
+    - \ref MD4
+    - \ref MD5
+    - \ref Password
+    - \ref PKCS7
+    - \ref Poly1305
+    - \ref Rabbit
+    - \ref RIPEMD
+    - \ref RSA
+    - \ref SHA
+    - \ref SRP
+*/
