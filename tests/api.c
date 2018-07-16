5de7a34fd432e99f0e9b0761c5c699894c26df36
Go Hosohara <go@wolfssl.com>
2018-07-16 17:19:17 +0900

    Add memory free to prevent from leaking

--- tests/api.c
+++ tests/api.c
@@ -18560,7 +18560,11 @@ static void test_wolfSSL_OBJ(void)
             AssertNotNull(asn1Name = X509_NAME_ENTRY_get_object(x509NameEntry));
             AssertTrue((nid = OBJ_obj2nid(asn1Name)) > 0);
             printf("nid=%d\n", nid);
+            ASN1_OBJECT_free(asn1Name);
         }
+        BIO_free(bio);
+        X509_free(x509);
+        XFCLOSE(fp);
     }
 
     for (i = 0; p12_f[i] != NULL; i++)
@@ -18578,7 +18582,11 @@ static void test_wolfSSL_OBJ(void)
             AssertNotNull(asn1Name = X509_NAME_ENTRY_get_object(x509NameEntry));
             AssertTrue((nid = OBJ_obj2nid(asn1Name)) > 0);
             printf("nid=%d\n", nid);
+            ASN1_OBJECT_free(asn1Name);
         }
+        BIO_free(bio);
+        X509_free(x509);
+        XFCLOSE(fp);
     }
 
     printf(resultFmt, passed);
