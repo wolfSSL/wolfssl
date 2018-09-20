452d602b345a22212e40bbf7b59aa949f7f36f95
MJSPollard <mpollard@wolfssl.com>
2018-09-20 14:40:53 -0600

    Added stub files to prevent fatal errors
    OPENSSL_STACK error now fixed

--- wolfssl/openssl/ssl.h
+++ wolfssl/openssl/ssl.h
@@ -87,6 +87,7 @@ typedef WOLFSSL_ASN1_OBJECT    ASN1_OBJECT;
 typedef WOLFSSL_ASN1_STRING    ASN1_STRING;
 typedef WOLFSSL_dynlock_value  CRYPTO_dynlock_value;
 typedef WOLFSSL_BUF_MEM        BUF_MEM;
+typedef WOLFSSL_STACK          OPENSSL_STACK;
 
 /* GENERAL_NAME and BASIC_CONSTRAINTS structs may need implemented as
  * compatibility layer expands. For now treating them as an ASN1_OBJECT */
