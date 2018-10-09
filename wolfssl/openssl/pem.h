ac49f87a47989fcae9c65c139008b6ac03aab330
Aaron Jense <aaron@wolfssl.com>
2018-10-09 10:01:05 -0600

    Fix compile errors with DSA/EC_PUBKEY and minor formatting

--- wolfssl/openssl/pem.h
+++ wolfssl/openssl/pem.h
@@ -124,9 +124,8 @@ int wolfSSL_PEM_write_bio_ECPrivateKey(WOLFSSL_BIO* bio, WOLFSSL_EC_KEY* ec,
                                        pem_password_cb* cb, void* arg);
 WOLFSSL_API
 WOLFSSL_EC_KEY* wolfSSL_PEM_read_bio_ECPrivateKey(WOLFSSL_BIO* bio,
-                                                    WOLFSSL_EC_KEY** ec,
-                                                    pem_password_cb* cb,
-                                                    void *u);
+                                                  WOLFSSL_EC_KEY** ec,
+                                                  pem_password_cb* cb,void *u);
 WOLFSSL_API
 int wolfSSL_PEM_write_mem_ECPrivateKey(WOLFSSL_EC_KEY* key,
                                        const EVP_CIPHER* cipher,
