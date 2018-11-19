3a033cae8cd771610cc821f2a1e9f321281b6001
Tesfa Mael <tesfa@wolfssl.com>
2018-11-19 08:59:48 -0800

    Add standard GPLv2 header

--- IDE/ECLIPSE/MICRIUM/user_settings.h
+++ IDE/ECLIPSE/MICRIUM/user_settings.h
@@ -1,3 +1,24 @@
+/* user_setting.h
+ *
+ * Copyright (C) 2018 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
 #ifndef MICRIUM_USER_SETTINGS_H_
 #define MICRIUM_USER_SETTINGS_H_
 
@@ -13,7 +34,7 @@
 #define WOLFSSL_CLIENT_TEST
 #define WOLFSSL_SERVER_TEST
 
-/* adjust x to seconds since Jan 01 1970. (UTC) 
+/* adjust x to seconds since Jan 01 1970. (UTC)
 https://www.unixtimestamp.com/
 */
 #define CURRENT_UNIX_TS 1542605837
