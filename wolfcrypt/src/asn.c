8ddf072a6c9b10500c949370a24004add9061f0f
Carie Pointer <carie@wolfssl.com>
2018-12-13 16:51:47 -0700

    Fix for some EC keys

--- wolfcrypt/src/asn.c
+++ wolfcrypt/src/asn.c
@@ -9428,6 +9428,8 @@ int PemToDer(const unsigned char* buff, long longSz, int type,
             der->length = ret;
             if (algId == DSAk)
                 *keyFormat = 2;
+            else if (algId == ECDSAk)
+                *keyFormat = 1;
         }
         else {
             /* ignore failure here and assume key is not pkcs8 wrapped */
