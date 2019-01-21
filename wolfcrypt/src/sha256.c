9fc0610720da2e4ee23d24f929345e0d2956ba2c
David Garske <david@wolfssl.com>
2019-01-21 15:52:33 -0800

    Fix to ensure hash devCtx is cleared.

--- wolfcrypt/src/sha256.c
+++ wolfcrypt/src/sha256.c
@@ -543,6 +543,7 @@ static int InitSha256(wc_Sha256* sha256)
         sha256->heap = heap;
     #ifdef WOLF_CRYPTO_CB
         sha256->devId = devId;
+        sha256->devCtx = NULL;
     #endif
 
         ret = InitSha256(sha256);
