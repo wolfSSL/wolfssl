ec4dad76e25dc4aad39e11bb96579437ca6c998a
Tesfa Mael <tesfa@wolfssl.com>
2019-05-21 16:15:27 -0700

    send protocol_version fatal alert message

--- src/internal.c
+++ src/internal.c
@@ -7259,7 +7259,11 @@ static int GetRecordHeader(WOLFSSL* ssl, const byte* input, word32* inOutIdx,
             WOLFSSL_MSG("SSL version error");
             /* send alert per RFC5246 Appendix E. Backward Compatibility */
             if (ssl->options.side == WOLFSSL_CLIENT_END) {
+#ifdef WOLFSSL_MYSQL_COMPATIBLE
+                SendAlert(ssl, alert_fatal, wc_protocol_version);
+#else
                 SendAlert(ssl, alert_fatal, protocol_version);
+#endif
             }
             return VERSION_ERROR;              /* only use requested version */
         }
