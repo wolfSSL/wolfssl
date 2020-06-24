fe7d9ea7c1865bdc5a1320f48b28eb342449fcdc
Ethan Looney <ethan@wolfssl.com>
2020-06-24 12:12:12 -0700

    changed RNG to WC_RNG

--- tests/api.c
+++ tests/api.c
@@ -16114,7 +16114,7 @@ static int test_wc_curve448_make_key (void)
 {
     int ret = 0;
 #if defined(HAVE_CURVE448)
-    RNG           rng;
+    WC_RNG           rng;
     curve448_key  key;
     int           keysize;
     
@@ -16174,7 +16174,7 @@ static int test_wc_curve448_import_private_raw_ex(void)
 {
     int ret = 0;
 #if defined(HAVE_CURVE448)
-    RNG             rng; 
+    WC_RNG             rng; 
     curve448_key    key;
     byte            priv[CURVE448_KEY_SIZE];
     byte            pub[CURVE448_KEY_SIZE];
