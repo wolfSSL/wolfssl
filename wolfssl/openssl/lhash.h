6b3a460c1d039d0b9863818f7334a1f80df16eae
Juliusz Sosinowicz <juliusz@wolfssl.com>
2020-07-25 00:47:59 +0200

    OpenSSL Compat layer
    Implement WOLFSSL_CONF_VALUE:
    - wolfSSL_CONF_VALUE_new
    - wolfSSL_CONF_VALUE_new_values
    - wolfSSL_CONF_add_string
    - wolfSSL_X509V3_conf_free
    - wolfSSL_sk_CONF_VALUE_push
    - wolfSSL_NCONF_load
    - wolfSSL_NCONF_free
    - wolfSSL_CONF_new_section
    - wolfSSL_CONF_get_section
    Implment some buffer functions
    - wolfSSL_strlcat
    - wolfSSL_strlcpy

--- wolfssl/openssl/lhash.h
+++ wolfssl/openssl/lhash.h
@@ -32,22 +32,27 @@
 
 #ifdef OPENSSL_ALL
 #define IMPLEMENT_LHASH_HASH_FN(name, type) \
-    unsigned long name##_LHASH_HASH(const void *arg) \
-    {                                                \
-        const o_type *a = arg;                       \
-        return name##_hash(a);                       \
+    unsigned long wolfSSL_##name##_LHASH_HASH(const void *arg) \
+    {                                                          \
+        const type *a = arg;                                 \
+        return name##_hash(a);                                 \
     }
 #define IMPLEMENT_LHASH_COMP_FN(name, type) \
-    int name##_LHASH_COMP(const void *p1, const void *p2) \
-    {                                                     \
-        const type *_p1 = p1;                             \
-        const type *_p2 = p2;                             \
-        return name##_cmp(_p1, _p2);                      \
+    int wolfSSL_##name##_LHASH_COMP(const void *p1, const void *p2) \
+    {                                                               \
+        const type *_p1 = p1;                                       \
+        const type *_p2 = p2;                                       \
+        return name##_cmp(_p1, _p2);                                \
     }
+
+#define LHASH_HASH_FN(name) wolfSSL_##name##_LHASH_HASH
+#define LHASH_COMP_FN(name) wolfSSL_##name##_LHASH_COMP
 
 WOLFSSL_API unsigned long wolfSSL_LH_strhash(const char *str);
 
 WOLFSSL_API void *wolfSSL_lh_retrieve(WOLFSSL_STACK *sk, void *data);
+
+#define lh_strhash          wolfSSL_LH_strhash
 
 #endif
 
