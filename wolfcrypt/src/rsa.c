b8147c704bbfa3005cf6fb3bc1cf6287e86a851e
Sean Parkinson <sean@wolfssl.com>
2020-08-03 12:17:03 +1000

    RSA PSS check input length is hash length
    Input is the hash of the message and the hash type is the hash used to
    generate the hash/input.

--- wolfcrypt/src/rsa.c
+++ wolfcrypt/src/rsa.c
@@ -1129,6 +1129,9 @@ static int RsaPad_PSS(const byte* input, word32 inputLen, byte* pkcsBlock,
     hLen = wc_HashGetDigestSize(hType);
     if (hLen < 0)
         return hLen;
+    if ((int)inputLen != hLen) {
+        return BAD_FUNC_ARG;
+    }
 
     hiBits = (bits - 1) & 0x7;
     if (hiBits == 0) {
