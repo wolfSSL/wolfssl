43e78a343fcdaea39a57dc4eb57b08ed12a49d1f
David Garske <david@wolfssl.com>
2020-08-07 16:56:58 -0700

    Fix state machine after script rebase.

--- wolfcrypt/src/sp_x86_64.c
+++ wolfcrypt/src/sp_x86_64.c
@@ -23267,7 +23267,7 @@ int sp_ecc_verify_256_nb(sp_ecc_ctx_t* sp_ctx, const byte* hash, word32 hashLen,
         break;
     case 4: /* NORMS3 */
         sp_256_mont_mul_order_4(ctx->u1, ctx->u1, ctx->s);
-        ctx->state = 4;
+        ctx->state = 5;
         break;
     case 5: /* NORMS4 */
         sp_256_mont_mul_order_4(ctx->u2, ctx->u2, ctx->s);
@@ -30254,7 +30254,7 @@ int sp_ecc_verify_384_nb(sp_ecc_ctx_t* sp_ctx, const byte* hash, word32 hashLen,
         break;
     case 4: /* NORMS3 */
         sp_384_mont_mul_order_6(ctx->u1, ctx->u1, ctx->s);
-        ctx->state = 4;
+        ctx->state = 5;
         break;
     case 5: /* NORMS4 */
         sp_384_mont_mul_order_6(ctx->u2, ctx->u2, ctx->s);
