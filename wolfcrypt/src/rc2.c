b58ea5842aacd8519d2a462ab186e2df597667a6
David Garske <david@wolfssl.com>
2020-09-18 17:04:26 -0700

    wolfSSL RC2 template.

---
+++ wolfcrypt/src/rc2.c
@@ -0,0 +1,43 @@
+/* rc2.c
+ *
+ * Copyright (C) 2006-2020 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+/*
+
+DESCRIPTION
+This library provides the interface to the RC2 encryption algorithm.
+
+*/
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#include <wolfssl/wolfcrypt/settings.h>
+
+#ifdef WC_RC2
+
+#include <wolfssl/wolfcrypt/rc2.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+
+
+
+
+#endif /* WC_RC2 */
