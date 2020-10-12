f1bd79ac5044c45a7410f113d4430fd8c130af87
John Safranek <john@wolfssl.com>
2020-10-12 16:06:20 -0700

    FIPS 140-3 1. Added enable option for FIPS 140-3 in configure script. 2. Modify DES3 source to disallow DES3 for the new option. 3. Added the new constants to fips_test.h. 4. Added some new test functions. 5. Added API for doing the POST. 6. Added a processing state for the CASTs. 7. Delete some unused prototypes from FIPS test API.

--- wolfssl/wolfcrypt/fips_test.h
+++ wolfssl/wolfcrypt/fips_test.h
@@ -31,8 +31,35 @@
     extern "C" {
 #endif
 
-/* Known Answer Test string inputs are hex, internal */
-WOLFSSL_LOCAL int DoKnownAnswerTests(char*, int);
+enum FipsCastId {
+    FIPS_CAST_AES_CBC,
+    FIPS_CAST_AES_GCM,
+    FIPS_CAST_HMAC_SHA1,
+    FIPS_CAST_HMAC_SHA2_256,
+    FIPS_CAST_HMAC_SHA2_512,
+    FIPS_CAST_HMAC_SHA3_256,
+    FIPS_CAST_DRBG,
+    FIPS_CAST_RSA_SIGN_PKCS1v15,
+    FIPS_CAST_ECC_CDH,
+    FIPS_CAST_ECC_PRIMITIVE_Z,
+    FIPS_CAST_DH_PRIMITIVE_Z,
+    FIPS_CAST_ECDSA_PAIRWISE,
+    FIPS_CAST_COUNT
+};
+
+enum FipsCastStateId {
+    FIPS_CAST_STATE_INIT,
+    FIPS_CAST_STATE_PROCESSING,
+    FIPS_CAST_STATE_SUCCESS,
+    FIPS_CAST_STATE_FAILURE
+};
+
+enum FipsModeId {
+    FIPS_MODE_INIT,
+    FIPS_MODE_NORMAL,
+    FIPS_MODE_DEGRADED,
+    FIPS_MODE_FAILED
+};
 
 
 /* FIPS failure callback */
@@ -49,6 +76,11 @@ WOLFSSL_API const char* wolfCrypt_GetCoreHash_fips(void);
     /* Public function to force failure mode for operational testing */
     WOLFSSL_API int wolfCrypt_SetStatus_fips(int);
 #endif
+
+WOLFSSL_LOCAL int DoIntegrityTest(char*, int);
+WOLFSSL_API int wc_GetCastStatus_fips(int);
+WOLFSSL_LOCAL int DoPOST(char*, int);
+WOLFSSL_LOCAL int DoCAST(int);
 
 
 #ifdef __cplusplus
