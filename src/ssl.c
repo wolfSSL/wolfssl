649abf2332582d8f3b47e574853ad6847916896a
Juliusz Sosinowicz <juliusz@wolfssl.com>
2020-10-13 16:21:27 +0200

    check null

--- src/ssl.c
+++ src/ssl.c
@@ -42053,6 +42053,10 @@ err:
         int ret;
 
         WOLFSSL_ENTER("wolfSSL_CTX_use_certificate");
+        if (!ctx || !x || !x->derCert) {
+            WOLFSSL_MSG("Bad parameter");
+            return WOLFSSL_FAILURE;
+        }
 
         FreeDer(&ctx->certificate); /* Make sure previous is free'd */
         ret = AllocDer(&ctx->certificate, x->derCert->length, CERT_TYPE,
