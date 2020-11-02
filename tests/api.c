48073fb678096bd0e9f79fe4d7b93289467640ce
Ethan Looney <ethan@wolfssl.com>
2020-11-02 14:22:01 -0700

    Removed unnecessary test

--- tests/api.c
+++ tests/api.c
@@ -29205,11 +29205,6 @@ static int test_wolfSSL_GetLoggingCb (void)
         if (wolfSSL_GetLoggingCb() == NULL) {
             ret = 0;
         }
-        if (ret == 0) {
-            if (wolfSSL_GetLoggingCb() != NULL) {
-                ret = -1;
-            }
-        }
     }
 
     printf(resultFmt, ret == 0 ? passed : failed);
