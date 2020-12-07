6fcc6d4c0c42a617fb5c3fdb0b7f7bdefbd1a333
WKJay <1931048074@qq.com>
2020-12-07 16:22:28 +0800

    Port for RT-Thread

--- wolfssl/internal.h
+++ wolfssl/internal.h
@@ -146,6 +146,8 @@
     /* do nothing, just don't pick Unix */
 #elif defined(FREERTOS) || defined(FREERTOS_TCP) || defined(WOLFSSL_SAFERTOS)
     /* do nothing */
+#elif defined(RTTHREAD)
+    /* do nothing */
 #elif defined(EBSNET)
     /* do nothing */
 #elif defined(FREESCALE_MQX) || defined(FREESCALE_KSDK_MQX)
