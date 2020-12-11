04e22b0747a41bf56ca0dfce3d15e208720c7ef9
Jacob Barthelmeh <jacob@wolfssl.com>
2020-12-11 10:00:11 +0700

    add restriction to excluded DIR name constraint

--- certs/test/gen-ext-certs.sh
+++ certs/test/gen-ext-certs.sh
@@ -128,3 +128,35 @@ countryName = US
 EOF
 gen_cert
 
+OUT=certs/test/cert-ext-ndir-exc.der
+KEYFILE=certs/ca-key.der
+CONFIG=certs/test/cert-ext-ndir-exc.cfg
+tee >$CONFIG <<EOF
+[ req ]
+distinguished_name = req_distinguished_name
+prompt             = no
+x509_extensions    = constraints
+
+[ req_distinguished_name ]
+C             = US
+ST            = Montana
+L             = Bozeman
+O             = Sawtooth
+OU            = Consulting
+CN            = www.wolfssl.com
+emailAddress  = info@wolfsssl.com
+
+[constraints]
+subjectKeyIdentifier=hash
+authorityKeyIdentifier=keyid:always,issuer:always
+basicConstraints=CA:TRUE
+nameConstraints=critical,excluded;dirName:dir_name_exclude
+
+[dir_name_exclude]
+countryName = US
+stateOrProvinceName = California
+
+EOF
+gen_cert
+
+
