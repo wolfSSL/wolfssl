eb6473b00f74a28798a2e39d4bb71a09ae23e923
Hayden Roche <hayden@wolfssl.com>
2020-12-15 09:13:32 -0600

    Fix bugs that made it so client side wasn't verifying certificate status.

--- wolfssl/internal.h
+++ wolfssl/internal.h
@@ -2352,7 +2352,7 @@ typedef struct {
     union {
         OcspRequest ocsp;
     } request;
-#if defined(WOLFSSL_TLS13) && !defined(NO_WOLFSSL_SERVER)
+#if defined(WOLFSSL_TLS13)
     buffer response;
 #endif
 } CertificateStatusRequest;
