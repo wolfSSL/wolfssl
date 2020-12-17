e41fe178d3181104030717cfcc4de87289e2485a
WKJay <1931048074@qq.com>
2020-12-17 08:42:53 +0800

    Add include.am

--- IDE/ECLIPSE/RTTHREAD/wolfssl_test.c
+++ IDE/ECLIPSE/RTTHREAD/wolfssl_test.c
@@ -22,9 +22,12 @@
 #include <wolfcrypt/test/test.h>
 #include <wolfcrypt/benchmark/benchmark.h>
 
-int wolfssl_test(void){
-     wolfcrypt_test(NULL);
-     benchmark_test(NULL);
-     return 0;
+int wolfssl_test(void) {
+#if !defined(NO_CRYPT_TEST)
+    wolfcrypt_test(NULL);
+#endif
+#if !defined(NO_CRYPT_BENCHMARK)
+    benchmark_test(NULL);
+#endif
+    return 0;
 }
-
