47c186df34ef8e8ca9195e1573fab6f536a0affd
Jacob Barthelmeh <jacob@wolfssl.com>
2020-12-22 02:33:58 +0700

    prepare for release 4.6.0

--- wolfssl/version.h
+++ wolfssl/version.h
@@ -28,8 +28,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFSSL_VERSION_STRING "4.5.0"
-#define LIBWOLFSSL_VERSION_HEX 0x04005000
+#define LIBWOLFSSL_VERSION_STRING "4.6.0"
+#define LIBWOLFSSL_VERSION_HEX 0x04006000
 
 #ifdef __cplusplus
 }
