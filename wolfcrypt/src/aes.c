8b922c3f333019c111610a25db531c272257668f
Daniel Pouzzner <douzzer@wolfssl.com>
2021-02-08 21:50:29 -0600

    aes.c: fix overwide code text in wc_AesCcmEncrypt().

--- wolfcrypt/src/aes.c
+++ wolfcrypt/src/aes.c
@@ -7883,8 +7883,8 @@ int wc_AesCcmEncrypt(Aes* aes, byte* out, const byte* in, word32 inSz,
     const word32 wordSz = (word32)sizeof(word32);
 
     /* sanity check on arguments */
-    if (aes == NULL || (inSz != 0 && (in == NULL || out == NULL)) || nonce == NULL
-            || authTag == NULL || nonceSz < 7 || nonceSz > 13 ||
+    if (aes == NULL || (inSz != 0 && (in == NULL || out == NULL)) ||
+        nonce == NULL || authTag == NULL || nonceSz < 7 || nonceSz > 13 ||
             authTagSz > AES_BLOCK_SIZE)
         return BAD_FUNC_ARG;
 
