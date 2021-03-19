1c81afcc0a31482ffce7751502e8db82c69fc283
TakayukiMatsuo <tak@wolfssl.com>
2021-03-19 14:23:40 +0900

    Add wc_FreeDhKey in wolfSSL_d2i_PUBKEY

--- src/ssl.c
+++ src/ssl.c
@@ -7945,6 +7945,7 @@ WOLFSSL_EVP_PKEY* wolfSSL_d2i_PUBKEY(WOLFSSL_EVP_PKEY** out,
             }
             wolfSSL_EVP_PKEY_free(pkey);
         }
+        wc_FreeDhKey(&dh);
     }
     #endif /* !HAVE_FIPS || HAVE_FIPS_VERSION > 2 */
     #endif /* !NO_DH &&  OPENSSL_EXTRA && WOLFSSL_DH_EXTRA */
