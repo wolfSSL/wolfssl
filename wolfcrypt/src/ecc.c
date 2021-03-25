133faea89a1d863aafc937c2ded44d43c2d04ed9
John Safranek <john@wolfssl.com>
2021-03-24 17:48:48 -0700

    Hushed compiler warnings about unused variables.

--- wolfcrypt/src/ecc.c
+++ wolfcrypt/src/ecc.c
@@ -8565,7 +8565,9 @@ static int _ecc_validate_public_key(ecc_key* key, int partial, int priv)
     FREE_CURVE_SPECS();
 #endif /* WOLFSSL_ATECC508A */
 #else
-    err = WC_KEY_SIZE_E;
+    (void)partial;
+    (void)priv;
+    return WC_KEY_SIZE_E;
 #endif /* !WOLFSSL_SP_MATH */
     return err;
 }
