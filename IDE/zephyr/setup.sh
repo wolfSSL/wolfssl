70fc37a2340c17e44212fedd2b86d956a7114384
Sean Parkinson <sean@wolfssl.com>
2021-07-05 21:52:20 +1000

    Zephyr port: fix setup.sh script
    Apply diff to include wolfssl as a zephyr module

--- IDE/zephyr/setup.sh
+++ IDE/zephyr/setup.sh
@@ -14,10 +14,18 @@ if [ ! -d $ZEPHR_DIR ]; then
 fi
 
 cd `dirname $0`
+DIFF_FILE=`pwd`/wolfssl_zephyr.diff
 
 (cd lib; ./install_lib.sh $ZEPHYR_DIR)
 (cd module; ./install_module.sh $ZEPHYR_DIR)
 (cd wolfssl_test; ./install_test.sh $ZEPHYR_DIR)
 (cd wolfssl_tls_sock; ./install_sample.sh $ZEPHYR_DIR)
 (cd wolfssl_tls_thread; ./install_sample.sh $ZEPHYR_DIR)
+echo
+echo "IMPORTANT"
+echo "Add the following lines to the end of 'projects' in:"
+echo "  $ZEPHYR_DIR/zephyr/west.yml:"
+echo "    - name: wolfssl"
+echo "      path: modules/crypto/wolfssl"
+
 
