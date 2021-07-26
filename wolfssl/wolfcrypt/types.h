917fdfbaf7025a8e0513a7cbac2ce982782761f9
David Garske <david@wolfssl.com>
2021-07-26 15:39:34 -0700

    Peer review fix (second try)

--- wolfssl/wolfcrypt/types.h
+++ wolfssl/wolfcrypt/types.h
@@ -293,7 +293,6 @@ decouple library dependencies with standard string, memory and so on.
         /* GCC 7 has new switch() fall-through detection */
         #if defined(__GNUC__)
             #if ((__GNUC__ > 7) || ((__GNUC__ == 7) && (__GNUC_MINOR__ >= 1)))
-                #undef  FALL_THROUGH
                 #if defined(WOLFSSL_LINUXKM) && defined(fallthrough)
                     #define FALL_THROUGH fallthrough
                 #else
@@ -304,6 +303,7 @@ decouple library dependencies with standard string, memory and so on.
     #endif /* FALL_THROUGH */
     #if !defined(FALL_THROUGH) || defined(__XC32)
         /* use stub for fall through by default or for Microchip compiler */
+        #undef  FALL_THROUGH
         #define FALL_THROUGH
     #endif
 
