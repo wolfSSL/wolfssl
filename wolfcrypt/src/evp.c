447705a2cbb9b47fa48d3838c023677295e1583c
Hideki Miyazaki <hide@wolfssl.com>
2021-07-30 10:21:16 +0900

    fix jenkins failure

--- wolfcrypt/src/evp.c
+++ wolfcrypt/src/evp.c
@@ -4005,7 +4005,7 @@ int wolfSSL_EVP_MD_type(const WOLFSSL_EVP_MD *md)
         md.fn = fn;
         md.arg = args;
         
-        return wolfSSL_OBJ_NAME_do_all(WOLFSSL_OBJ_NAME_TYPE_MD_METH,
+        wolfSSL_OBJ_NAME_do_all(WOLFSSL_OBJ_NAME_TYPE_MD_METH,
                         md_do_all_func, &md);
     }
     
