c6cabba0dabe9b68bdd1d34300950563b2e6feef
Daniel Pouzzner <douzzer@wolfssl.com>
2021-08-05 16:47:57 -0500

    linuxkm/pie_redirect_table.c: add assert for defined(__PIE__), and conditionalize the PIE dummy table variable name on architecture.

--- linuxkm/pie_redirect_table.c
+++ linuxkm/pie_redirect_table.c
@@ -19,6 +19,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
  */
 
+#ifndef __PIE__
+    #error pie_redirect_table.c must be compiled -fPIE.
+#endif
+
 #ifdef HAVE_CONFIG_H
     #include <config.h>
 #endif
@@ -27,15 +31,20 @@
 #include <wolfssl/wolfcrypt/error-crypt.h>
 #include <wolfssl/ssl.h>
 
-/* compiling -fPIE results in references to the GOT, which remain after linking
+/* compiling -fPIE results in references to the GOT or equivalent thereof, which remain after linking
  * even if all other symbols are resolved by the link.  naturally there is no
  * GOT in the kernel, and the wolfssl Kbuild script explicitly checks that no
  * GOT relocations occur in the PIE objects, but we still need to include a
  * dummy value here, scoped to the module, to eliminate the otherwise unresolved
  * symbol.
  */
-extern void *_GLOBAL_OFFSET_TABLE_;
-void *_GLOBAL_OFFSET_TABLE_ = 0;
+#if defined(CONFIG_X86)
+    extern void *_GLOBAL_OFFSET_TABLE_;
+    void *_GLOBAL_OFFSET_TABLE_ = 0;
+#elif defined(CONFIG_MIPS)
+  extern void *gp_disp;
+  void *_gp_disp = 0;
+#endif
 
 struct wolfssl_linuxkm_pie_redirect_table wolfssl_linuxkm_pie_redirect_table;
 
