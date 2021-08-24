dae4d637c9594fda9745ba63b7150e428d1e9d3b
Chris Conlon <chris@wolfssl.com>
2021-08-24 09:29:12 -0600

    define SSL_OP_* in openssl/ssl.h for compatibility

--- wolfssl/openssl/ssl.h
+++ wolfssl/openssl/ssl.h
@@ -1385,6 +1385,12 @@ wolfSSL_X509_STORE_set_verify_cb((WOLFSSL_X509_STORE *)(s), (WOLFSSL_X509_STORE_
 #define SSL_CTRL_SET_TLSEXT_TICKET_KEY_CB 72
 #endif
 
+/* Some openssl consumers try to detect these options with ifdef, defining
+ * here since we use an enum internally instead */
+#define SSL_OP_SINGLE_DH_USE            SSL_OP_SINGLE_DH_USE
+#define SSL_OP_SINGLE_ECDH_USE          SSL_OP_SINGLE_ECDH_USE
+#define SSL_OP_CIPHER_SERVER_PREFERENCE SSL_OP_CIPHER_SERVER_PREFERENCE
+
 #define OPENSSL_config	                wolfSSL_OPENSSL_config
 #define OPENSSL_memdup                  wolfSSL_OPENSSL_memdup
 #define OPENSSL_cleanse                 wolfSSL_OPENSSL_cleanse
