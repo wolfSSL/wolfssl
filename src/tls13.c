ff9fed08a336a7259b684477ec57ee6368cb8f8e
Jacob Barthelmeh <jacob@wolfssl.com>
2021-08-26 21:17:45 -0600

    fix count on number of tickets sent

--- src/tls13.c
+++ src/tls13.c
@@ -9428,8 +9428,8 @@ int wolfSSL_accept_TLSv13(WOLFSSL* ssl)
                     WOLFSSL_ERROR(ssl->error);
                     return WOLFSSL_FATAL_ERROR;
                 }
+                ssl->options.ticketsSent = 1;
             }
-            ssl->options.ticketsSent = 1;
     #endif
 #endif /* HAVE_SESSION_TICKET */
             ssl->options.acceptState = TLS13_PRE_TICKET_SENT;
