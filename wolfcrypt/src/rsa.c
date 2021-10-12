b577984574dbb06992a9bbece9df32339fc4f7aa
Daniel Pouzzner <douzzer@wolfssl.com>
2021-10-12 11:53:26 -0500

    rsa.c: fix whitespace.

--- wolfcrypt/src/rsa.c
+++ wolfcrypt/src/rsa.c
@@ -639,12 +639,12 @@ static int _ifc_pairwise_consistency_test(RsaKey* key, WC_RNG* rng)
 #ifdef WOLFSSL_ASYNC_CRYPT
     } while (ret == WC_PENDING_E);
 #endif
-    
+
     if (ret > 0) {
         sigLen = (word32)ret;
 #ifdef WOLFSSL_ASYNC_CRYPT
         /* Do blocking async calls here, caller does not support WC_PENDING_E */
-        do {    
+        do {
             if (ret == WC_PENDING_E)
                 ret = wc_AsyncWait(ret, &key->asyncDev, WC_ASYNC_FLAG_CALL_AGAIN);
             if (ret >= 0)
