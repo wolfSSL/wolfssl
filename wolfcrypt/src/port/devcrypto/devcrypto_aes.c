wolfSSL/wolfssl

commit a718637c6f03f6f7e480b6192a43e1e790599b7c

AES: harmonize wc_Aes{Encrypt,Decrypt} and wc_Aes{Encrypt,Decrypt}Direct implementations to return int; add return values to all static void functions in aes.c that can fail; add WARN_UNUSED_RESULT to all static functions in aes.c with return values; implement missing error percolation around AES block cipher implementations; bump FIPS version for v5-ready and v5-dev to 5.3 (v5-RC12 is 5.2).

wolfcrypt/src/port/devcrypto/devcrypto_aes.c
@@ -169,15 +169,15 @@ static int wc_DevCrypto_AesDirect(Aes* aes, byte* out, const byte* in,
 
 
 #if defined(WOLFSSL_AES_DIRECT) || defined(HAVE_AESCCM)
-void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
+int wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
 {
-    wc_DevCrypto_AesDirect(aes, out, in, AES_BLOCK_SIZE, COP_ENCRYPT);
+    return wc_DevCrypto_AesDirect(aes, out, in, AES_BLOCK_SIZE, COP_ENCRYPT);
 }
 
 
-void wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
+int wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
 {
-    wc_DevCrypto_AesDirect(aes, out, in, AES_BLOCK_SIZE, COP_DECRYPT);
+    return wc_DevCrypto_AesDirect(aes, out, in, AES_BLOCK_SIZE, COP_DECRYPT);
 }
 
 
@@ -208,6 +208,7 @@ int wc_AesCtrEncrypt(Aes* aes, byte* out, const byte* in, word32 sz)
     int ret;
     struct crypt_op crt;
     byte* tmp;
+    int ret;
 
     if (aes == NULL || out == NULL || in == NULL) {
         return BAD_FUNC_ARG;
@@ -253,9 +254,11 @@ int wc_AesCtrEncrypt(Aes* aes, byte* out, const byte* in, word32 sz)
     /* create key stream for later if needed */
     if (sz > 0) {
         Aes tmpAes;
-        wc_AesSetKey(&tmpAes, (byte*)aes->devKey, aes->keylen, (byte*)aes->reg,
-                AES_ENCRYPTION);
-        wc_AesEncryptDirect(&tmpAes, (byte*)aes->tmp, (const byte*)aes->reg);
+        if ((ret = wc_AesSetKey(&tmpAes, (byte*)aes->devKey, aes->keylen, (byte*)aes->reg,
+                                AES_ENCRYPTION)) != 0)
+            return ret;
+        if ((ret = wc_AesEncryptDirect(&tmpAes, (byte*)aes->tmp, (const byte*)aes->reg)) != 0)
+            return ret;
         wc_AesFree(&tmpAes);
         IncrementAesCounter((byte*)aes->reg);
 

configure.ac
@@ -280,13 +280,13 @@ AS_CASE([$ENABLED_FIPS],
     [ready|v5-ready],[
         FIPS_VERSION="v5-ready"
         HAVE_FIPS_VERSION=5
-        HAVE_FIPS_VERSION_MINOR=2
+        HAVE_FIPS_VERSION_MINOR=3
         ENABLED_FIPS="yes"
     ],
     [dev|v5-dev],[
         FIPS_VERSION="v5-dev"
         HAVE_FIPS_VERSION=5
-        HAVE_FIPS_VERSION_MINOR=2
+        HAVE_FIPS_VERSION_MINOR=3
         ENABLED_FIPS="yes"
     ],
     [

doc/dox_comments/header_files/aes.h
@@ -214,6 +214,9 @@ WOLFSSL_API int wc_AesCtrEncrypt(Aes* aes, byte* out,
     use cases ECB mode is considered to be less secure. Please avoid using ECB
     API’s directly whenever possible
 
+    \return int integer values corresponding to wolfSSL error or success
+    status
+
     \param aes pointer to the AES object used to encrypt data
     \param out pointer to the output buffer in which to store the cipher
     text of the encrypted message
@@ -232,7 +235,7 @@ WOLFSSL_API int wc_AesCtrEncrypt(Aes* aes, byte* out,
     \sa wc_AesDecryptDirect
     \sa wc_AesSetKeyDirect
 */
-WOLFSSL_API void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in);
+WOLFSSL_API int wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in);
 
 /*!
     \ingroup AES
@@ -245,7 +248,8 @@ WOLFSSL_API void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in);
     ECB mode is considered to be less secure. Please avoid using ECB API’s
     directly whenever possible
 
-    \return none
+    \return int integer values corresponding to wolfSSL error or success
+    status
 
     \param aes pointer to the AES object used to encrypt data
     \param out pointer to the output buffer in which to store the plain
@@ -266,7 +270,7 @@ WOLFSSL_API void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in);
     \sa wc_AesEncryptDirect
     \sa wc_AesSetKeyDirect
  */
-WOLFSSL_API void wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in);
+WOLFSSL_API int wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in);
 
 /*!
     \ingroup AES

src/ssl.c
@@ -30798,7 +30798,15 @@ void wolfSSL_AES_encrypt(const unsigned char* input, unsigned char* output,
         return;
     }
 
+#if !defined(HAVE_SELFTEST) && \
+    (!defined(HAVE_FIPS) || (defined(FIPS_VERSION_GE) && FIPS_VERSION_GE(5,3)))
+    if (wc_AesEncryptDirect((Aes*)key, output, input) != 0) {
+        WOLFSSL_MSG("wc_AesEncryptDirect failed");
+        return;
+    }
+#else
     wc_AesEncryptDirect((Aes*)key, output, input);
+#endif
 }
 
 
@@ -30818,7 +30826,15 @@ void wolfSSL_AES_decrypt(const unsigned char* input, unsigned char* output,
         return;
     }
 
+#if !defined(HAVE_SELFTEST) && \
+    (!defined(HAVE_FIPS) || (defined(FIPS_VERSION_GE) && FIPS_VERSION_GE(5,3)))
+    if (wc_AesDecryptDirect((Aes*)key, output, input) != 0) {
+        WOLFSSL_MSG("wc_AesDecryptDirect failed");
+        return;
+    }
+#else
     wc_AesDecryptDirect((Aes*)key, output, input);
+#endif
 }
 #endif /* WOLFSSL_AES_DIRECT */
 
@@ -30948,11 +30964,13 @@ void wolfSSL_AES_cbc_encrypt(const unsigned char *in, unsigned char* out,
     if (enc == AES_ENCRYPT) {
         if (wc_AesCbcEncrypt(aes, out, in, (word32)len) != 0) {
             WOLFSSL_MSG("Error with AES CBC encrypt");
+            return;
         }
     }
     else {
         if (wc_AesCbcDecrypt(aes, out, in, (word32)len) != 0) {
             WOLFSSL_MSG("Error with AES CBC decrypt");
+            return;
         }
     }
 
@@ -31006,11 +31024,13 @@ void wolfSSL_AES_cfb128_encrypt(const unsigned char *in, unsigned char* out,
     if (enc == AES_ENCRYPT) {
         if (wc_AesCfbEncrypt(aes, out, in, (word32)len) != 0) {
             WOLFSSL_MSG("Error with AES CBC encrypt");
+            return;
         }
     }
     else {
         if (wc_AesCfbDecrypt(aes, out, in, (word32)len) != 0) {
             WOLFSSL_MSG("Error with AES CBC decrypt");
+            return;
         }
     }
 

wolfcrypt/src/cmac.c
@@ -116,19 +116,12 @@ int wc_InitCmac_ex(Cmac* cmac, const byte* key, word32 keySz,
         byte l[AES_BLOCK_SIZE];
 
         XMEMSET(l, 0, AES_BLOCK_SIZE);
-#ifdef WOLFSSL_LINUXKM
-        ret =
-#endif
-            wc_AesEncryptDirect(&cmac->aes, l, l);
-#ifdef WOLFSSL_LINUXKM
+        ret = wc_AesEncryptDirect(&cmac->aes, l, l);
         if (ret == 0) {
-#endif
             ShiftAndXorRb(cmac->k1, l);
             ShiftAndXorRb(cmac->k2, cmac->k1);
             ForceZero(l, AES_BLOCK_SIZE);
-#ifdef WOLFSSL_LINUXKM
         }
-#endif
     }
     return ret;
 }
@@ -178,18 +171,11 @@ int wc_CmacUpdate(Cmac* cmac, const byte* in, word32 inSz)
             if (cmac->totalSz != 0) {
                 xorbuf(cmac->buffer, cmac->digest, AES_BLOCK_SIZE);
             }
-#ifdef WOLFSSL_LINUXKM
-            ret =
-#endif
-                wc_AesEncryptDirect(&cmac->aes, cmac->digest, cmac->buffer);
-#ifdef WOLFSSL_LINUXKM
+            ret = wc_AesEncryptDirect(&cmac->aes, cmac->digest, cmac->buffer);
             if (ret == 0) {
-#endif
                 cmac->totalSz += AES_BLOCK_SIZE;
                 cmac->bufferSz = 0;
-#ifdef WOLFSSL_LINUXKM
             }
-#endif
         }
     }
 
@@ -199,7 +185,7 @@ int wc_CmacUpdate(Cmac* cmac, const byte* in, word32 inSz)
 
 int wc_CmacFinal(Cmac* cmac, byte* out, word32* outSz)
 {
-    int ret = 0;
+    int ret;
     const byte* subKey;
 
     if (cmac == NULL || out == NULL || outSz == NULL) {
@@ -215,7 +201,6 @@ int wc_CmacFinal(Cmac* cmac, byte* out, word32* outSz)
         if (ret != CRYPTOCB_UNAVAILABLE)
             return ret;
         /* fall-through when unavailable */
-        ret = 0; /* reset error code */
     }
 #endif
 
@@ -236,17 +221,10 @@ int wc_CmacFinal(Cmac* cmac, byte* out, word32* outSz)
     }
     xorbuf(cmac->buffer, cmac->digest, AES_BLOCK_SIZE);
     xorbuf(cmac->buffer, subKey, AES_BLOCK_SIZE);
-#ifdef WOLFSSL_LINUXKM
-    ret =
-#endif
-        wc_AesEncryptDirect(&cmac->aes, cmac->digest, cmac->buffer);
-#ifdef WOLFSSL_LINUXKM
+    ret = wc_AesEncryptDirect(&cmac->aes, cmac->digest, cmac->buffer);
     if (ret == 0) {
-#endif
         XMEMCPY(out, cmac->digest, *outSz);
-#ifdef WOLFSSL_LINUXKM
     }
-#endif
 
     wc_AesFree(&cmac->aes);
     ForceZero(cmac, sizeof(Cmac));

wolfcrypt/src/port/af_alg/afalg_aes.c
@@ -301,19 +301,15 @@ static int wc_Afalg_AesDirect(Aes* aes, byte* out, const byte* in, word32 sz)
 
 
 #if defined(WOLFSSL_AES_DIRECT) && defined(WOLFSSL_AFALG)
-void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
+int wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
 {
-    if (wc_Afalg_AesDirect(aes, out, in, AES_BLOCK_SIZE) != 0) {
-        WOLFSSL_MSG("Error with AES encrypt direct call");
-    }
+    return wc_Afalg_AesDirect(aes, out, in, AES_BLOCK_SIZE);
 }
 
 
-void wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
+int wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
 {
-    if (wc_Afalg_AesDirect(aes, out, in, AES_BLOCK_SIZE) != 0) {
-        WOLFSSL_MSG("Error with AES decrypt direct call");
-    }
+    return wc_Afalg_AesDirect(aes, out, in, AES_BLOCK_SIZE);
 }
 
 
@@ -639,7 +635,9 @@ int wc_AesGcmEncrypt(Aes* aes, byte* out, const byte* in, word32 sz,
         XMEMCPY(initalCounter, iv, ivSz);
         initalCounter[AES_BLOCK_SIZE - 1] = 1;
         GHASH(aes, authIn, authInSz, out, sz, authTag, authTagSz);
-        wc_AesEncryptDirect(aes, scratch, initalCounter);
+        ret = wc_AesEncryptDirect(aes, scratch, initalCounter);
+        if (ret < 0)
+            return ret;
         xorbuf(authTag, scratch, authTagSz);
     }
 #else
@@ -786,7 +784,9 @@ int wc_AesGcmDecrypt(Aes* aes, byte* out, const byte* in, word32 sz,
         initalCounter[AES_BLOCK_SIZE - 1] = 1;
         tag = buf;
         GHASH(aes, NULL, 0, in, sz, tag, AES_BLOCK_SIZE);
-        wc_AesEncryptDirect(aes, scratch, initalCounter);
+        ret = wc_AesEncryptDirect(aes, scratch, initalCounter);
+        if (ret < 0)
+            return ret;
         xorbuf(tag, scratch, AES_BLOCK_SIZE);
         if (ret != 0) {
             return AES_GCM_AUTH_E;
@@ -836,7 +836,9 @@ int wc_AesGcmDecrypt(Aes* aes, byte* out, const byte* in, word32 sz,
     /* check on tag */
     if (authIn != NULL && authInSz > 0) {
         GHASH(aes, authIn, authInSz, in, sz, tag, AES_BLOCK_SIZE);
-        wc_AesEncryptDirect(aes, scratch, initalCounter);
+        ret = wc_AesEncryptDirect(aes, scratch, initalCounter);
+        if (ret < 0)
+            return ret;
         xorbuf(tag, scratch, AES_BLOCK_SIZE);
         if (ConstantCompare(tag, authTag, authTagSz) != 0) {
             return AES_GCM_AUTH_E;

wolfcrypt/src/port/arm/armv8-aes.c
@@ -5320,23 +5320,23 @@ int wc_AesGcmSetKey(Aes* aes, const byte* key, word32 len)
 /* AES-DIRECT */
 #if defined(WOLFSSL_AES_DIRECT)
         /* Allow direct access to one block encrypt */
-        void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
+        int wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
         {
             if (aes == NULL || out == NULL || in == NULL) {
                 WOLFSSL_MSG("Invalid input to wc_AesEncryptDirect");
-                return;
+                return BAD_FUNC_ARG;
             }
-            wc_AesEncrypt(aes, in, out);
+            return wc_AesEncrypt(aes, in, out);
         }
     #ifdef HAVE_AES_DECRYPT
         /* Allow direct access to one block decrypt */
-        void wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
+        int wc_AesDecryptDirect(Aes* aes, byte* out, const byte* in)
         {
             if (aes == NULL || out == NULL || in == NULL) {
                 WOLFSSL_MSG("Invalid input to wc_AesDecryptDirect");
                 return;
             }
-            wc_AesDecrypt(aes, in, out);
+            return wc_AesDecrypt(aes, in, out);
         }
     #endif /* HAVE_AES_DECRYPT */
 #endif /* WOLFSSL_AES_DIRECT */

commit 524760ea9f4383c4c786df7474604f5d84aee2af

devcrypto: remove duplicate definition of ret
Fixes: a718637c6

Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

wolfcrypt/src/port/devcrypto/devcrypto_aes.c
@@ -208,7 +208,6 @@ int wc_AesCtrEncrypt(Aes* aes, byte* out, const byte* in, word32 sz)
     int ret;
     struct crypt_op crt;
     byte* tmp;
-    int ret;
 
     if (aes == NULL || out == NULL || in == NULL) {
         return BAD_FUNC_ARG;