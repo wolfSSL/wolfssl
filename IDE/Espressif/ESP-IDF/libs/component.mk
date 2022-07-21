58aec606c6200180adf645290e3cbb6bfb0a68a5
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-07-21 08:12:54 +0200

    wolfSSL license, not wolfSSH

--- IDE/Espressif/ESP-IDF/libs/component.mk
+++ IDE/Espressif/ESP-IDF/libs/component.mk
@@ -1,20 +1,21 @@
 #
-#  Copyright (C) 2014-2022 wolfSSL Inc.
+# Copyright (C) 2006-2022 wolfSSL Inc.
 #
-# This file is part of wolfSSH.
+# This file is part of wolfSSL.
 #
-# wolfSSH is free software; you can redistribute it and/or modify
+# wolfSSL is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# wolfSSH is distributed in the hope that it will be useful,
+# wolfSSL is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with wolfSSH.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
 #
 #
 # Component Makefile
