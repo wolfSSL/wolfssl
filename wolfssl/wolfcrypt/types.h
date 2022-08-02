d283510aa99dcb94eb79973ce47e9b71521bba7f
Uriah Pollock <uriah@wolfssl.com>
2022-08-02 07:54:05 -0500

    Revert "Removed duplicate WOLFSSL_ABI tag."
    This reverts commit d17021bc4e8e3f12b0c0406f7fe400ed4c0bc2cd.

--- wolfssl/wolfcrypt/types.h
+++ wolfssl/wolfcrypt/types.h
@@ -39,6 +39,9 @@ decouple library dependencies with standard string, memory and so on.
     #endif
 
 
+    #define WOLFSSL_ABI
+            /* Tag for all the APIs that are a part of the fixed ABI. */
+
     /*
      * This struct is used multiple time by other structs and
      * needs to be defined somewhere that all structs can import
