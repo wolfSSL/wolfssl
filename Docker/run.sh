d55ef14cc7b949bf625a1ff9b656cc10e3ac1f5a
Andras Fekete <bandi13@users.noreply.github.com>
2022-12-14 13:17:22 -0500

    First crack at creating a common Docker environment

---
+++ Docker/run.sh
@@ -0,0 +1,4 @@
+echo "Running with \"${@}\"..."
+docker build -t wolfssl . && \
+docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure ${@} && make && ./testsuite/testsuite.test" && \
+docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash
