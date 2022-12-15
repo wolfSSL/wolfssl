be1b3ec0074aa9b2a7beb22be23dacfd03c460ed
Andras Fekete <bandi13@users.noreply.github.com>
2022-12-15 11:25:43 -0500

    Fix issue with multiple command arguments

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,4 +1,5 @@
 echo "Running with \"${@}\"..."
 docker build -t wolfssl . && \
-docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure ${@} && make && ./testsuite/testsuite.test" && \
+docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure $(echo ${@}) && make && ./testsuite/testsuite.test" && \
 docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash
+echo "Exited with error code $?"
