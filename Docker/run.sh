c1ad3457f32836991a1b731b5ad4377213abdb64
Andras Fekete <bandi13@users.noreply.github.com>
2022-12-15 12:10:25 -0500

    Run as non-root local user

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,5 +1,5 @@
 echo "Running with \"${@}\"..."
-docker build -t wolfssl . && \
+docker build -t wolfssl --build-arg UID=$(id -u) --build-arg GID=$(id -g) . && \
 docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure $(echo ${@}) && make && ./testsuite/testsuite.test" && \
 docker run -it -v $(pwd)/..:/tmp/wolfssl -w /tmp/wolfssl wolfssl /bin/bash
 echo "Exited with error code $?"
