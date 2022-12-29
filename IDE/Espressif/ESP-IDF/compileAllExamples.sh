f6bd696defaecca08cd7266a1ea193bace6b0124
Andras Fekete <andras@wolfssl.com>
2022-12-29 13:13:11 -0500

    Small updates to the script to support official ESP Docker images

--- IDE/Espressif/ESP-IDF/compileAllExamples.sh
+++ IDE/Espressif/ESP-IDF/compileAllExamples.sh
@@ -1,4 +1,7 @@
-./setup.sh
+#!/bin/bash
+
+SCRIPT_DIR=$(builtin cd ${BASH_SOURCE%/*}; pwd)
+pushd ${SCRIPT_DIR} && ./setup.sh; popd
 for file in "benchmark" "client" "server" "test"; do
     cd ${IDF_PATH}/examples/protocols/wolfssl_${file}/ && idf.py build
     if [ $? -ne 0 ]; then
