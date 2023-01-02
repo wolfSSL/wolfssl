b48d69f88e7a9b15904810c59dc7e0efd72cdead
Andras Fekete <andras@wolfssl.com>
2023-01-02 13:05:21 -0500

    Return an error value on failure

--- IDE/Espressif/ESP-IDF/compileAllExamples.sh
+++ IDE/Espressif/ESP-IDF/compileAllExamples.sh
@@ -9,6 +9,6 @@ for file in "benchmark" "client" "server" "test"; do
     cd ${IDF_PATH}/examples/protocols/wolfssl_${file}/ && idf.py build
     if [ $? -ne 0 ]; then
         echo "Failed in ${file}"
-        break
+        exit 1
     fi
 done
