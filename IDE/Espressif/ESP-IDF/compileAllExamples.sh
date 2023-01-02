bd5b444a97ea26f5b671ca40efcc44a2864295f7
Andras Fekete <andras@wolfssl.com>
2023-01-02 13:09:31 -0500

    Revert "Test out failure"
    This reverts commit 4652d7079a8d010977cb6863b1c537ee63b8f96f.

--- IDE/Espressif/ESP-IDF/compileAllExamples.sh
+++ IDE/Espressif/ESP-IDF/compileAllExamples.sh
@@ -6,7 +6,7 @@
 SCRIPT_DIR=$(builtin cd ${BASH_SOURCE%/*}; pwd)
 pushd ${SCRIPT_DIR} && ./setup.sh; popd
 for file in "benchmark" "client" "server" "test"; do
-    cd ${IDF_PATH}/examples/protocols/wolfssl_${file}/ && idf.py build2
+    cd ${IDF_PATH}/examples/protocols/wolfssl_${file}/ && idf.py build
     if [ $? -ne 0 ]; then
         echo "Failed in ${file}"
         exit 1
