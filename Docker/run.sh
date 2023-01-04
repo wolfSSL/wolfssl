adb406e1eebf05e452afca98fa9bf3ccd7abcfca
András Fekete <bandi13@users.noreply.github.com>
2023-01-04 11:48:24 -0500

    Adding some developer utilities (#5941)
    * Adding some developer utilities for Docker.
    * Add support for `make test`
    * Don't need to run the testsuite specifically
    * Share .gitconfig and ssh keys with the container
    
    Co-authored-by: Andras Fekete <andras@wolfssl.com>

--- Docker/run.sh
+++ Docker/run.sh
@@ -6,8 +6,8 @@ echo "Running with \"${*}\"..."
 WOLFSSL_DIR=$(builtin cd ${BASH_SOURCE%/*}/..; pwd)
 
 docker build -t wolfssl --build-arg UID=$(id -u) --build-arg GID=$(id -g) "${WOLFSSL_DIR}/Docker" && \
-  docker run -it -v "${WOLFSSL_DIR}:/tmp/wolfssl" -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure ${*@Q} && make && ./testsuite/testsuite.test" && \
-  docker run -it -v "${WOLFSSL_DIR}:/tmp/wolfssl" -w /tmp/wolfssl wolfssl /bin/bash
+  docker run -it -v ${HOME}/.gitconfig:/home/docker/.gitconfig:ro -v ${HOME}/.ssh:/home/docker/.ssh:ro -v "${WOLFSSL_DIR}:/tmp/wolfssl" -w /tmp/wolfssl wolfssl /bin/bash -c "./autogen.sh && ./configure ${*@Q} && make" && \
+  docker run -it -v ${HOME}/.gitconfig:/home/docker/.gitconfig:ro -v ${HOME}/.ssh:/home/docker/.ssh:ro -v "${WOLFSSL_DIR}:/tmp/wolfssl" -w /tmp/wolfssl wolfssl /bin/bash
 
 exitval=$?
 echo "Exited with error code $exitval"
