0c263a40983ee68e134a4acd4d262102c4d3d155
gojimmypi <jim@wolfssl.com>
2023-01-09 19:59:15 -0800

    tidy up ESP32S3 detection. known-good sdkconfig defaults

--- IDE/Espressif/ESP-IDF/examples/wolfssl_test/main/main.c
+++ IDE/Espressif/ESP-IDF/examples/wolfssl_test/main/main.c
@@ -151,18 +151,37 @@ void app_main(void)
 #endif
 
 
+    /* some interesting settings are target specific (ESP32, -C3, -S3, etc */
 #if defined(CONFIG_IDF_TARGET_ESP32C3)
     /* not available for C3 at this time */
+#elif defined(CONFIG_IDF_TARGET_ESP32S3)
+    ESP_LOGI(TAG, "CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ = %u MHz",
+                   CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ
+             );
+    ESP_LOGI(TAG, "Xthal_have_ccount = %u", Xthal_have_ccount);
 #else
-   // ESP_LOGI(TAG, "CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ = %u MHz", CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ);
+    ESP_LOGI(TAG, "CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ = %u MHz",
+                   CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ
+            );
     ESP_LOGI(TAG, "Xthal_have_ccount = %u", Xthal_have_ccount);
 #endif
+
+    /* all platforms: stack high water mark check */
     ESP_LOGI(TAG, "Stack HWM: %d\n", uxTaskGetStackHighWaterMark(NULL));
 
+    /* check to see if we are using hardware encryption */
 #if defined(NO_ESP32WROOM32_CRYPT)
     ESP_LOGI(TAG, "NO_ESP32WROOM32_CRYPT defined! HW acceleration DISABLED.");
 #else
-    ESP_LOGI(TAG, "ESP32WROOM32_CRYPT is enabled.");
+    #if defined(CONFIG_IDF_TARGET_ESP32C3)
+        #error "ESP32WROOM32_CRYPT not yet supported on ESP32-C3"
+    #elif defined(CONFIG_IDF_TARGET_ESP32S2)
+        #error "ESP32WROOM32_CRYPT not yet supported on ESP32-S2"
+    #elif defined(CONFIG_IDF_TARGET_ESP32S3)
+        #error "ESP32WROOM32_CRYPT not yet supported on ESP32-S3"
+    #else
+        ESP_LOGI(TAG, "ESP32WROOM32_CRYPT is enabled.");
+    #endif
 #endif
 
 
