2fd747dc519bc934ffc525398a0b745ca72dc0bd
gojimmypi <jim@wolfssl.com>
2023-02-02 09:08:58 -0800

    Examples no longer need setup

--- IDE/Espressif/ESP-IDF/compileAllExamples.sh
+++ IDE/Espressif/ESP-IDF/compileAllExamples.sh
@@ -4,7 +4,7 @@
 #   cd wolfssl && docker run --rm -v $PWD:/project -w /project espressif/idf:latest IDE/Espressif/ESP-IDF/compileAllExamples.sh
 
 SCRIPT_DIR=$(builtin cd ${BASH_SOURCE%/*}; pwd)
-pushd ${SCRIPT_DIR} && ./setup.sh; popd
+# pushd ${SCRIPT_DIR} && ./setup.sh; popd
 for file in "benchmark" "client" "server" "test"; do
     cd ${IDF_PATH}/examples/protocols/wolfssl_${file}/ && idf.py build
     if [ $? -ne 0 ]; then
