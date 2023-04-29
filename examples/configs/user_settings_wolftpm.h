07c1272389119e7ddb802286083f8a63454a8d33
John Bland <john.bland@wolfssl.com>
2023-04-28 20:17:46 -0400

    update example with minimum requirements for wolftpm's minimum

--- examples/configs/user_settings_wolftpm.h
+++ examples/configs/user_settings_wolftpm.h
@@ -38,91 +38,206 @@
 extern "C" {
 #endif
 
-#if 1
-    /* wolfTPM with TLS example (v1.3 only) */
-    #define WOLFSSL_TLS13
-    #define WOLFSSL_NO_TLS12
-    #define NO_OLD_TLS
+#undef  WOLFSSL_AES_NO_UNROLL
+#define WOLFSSL_AES_NO_UNROLL
 
-    #define HAVE_TLS_EXTENSIONS
-    #define HAVE_SUPPORTED_CURVES
-    #define HAVE_SERVER_RENEGOTIATION_INFO
-    #define HAVE_ENCRYPT_THEN_MAC
+#undef  WOLFSSL_AES_SMALL_TABLES
+#define WOLFSSL_AES_SMALL_TABLES
 
-    #define HAVE_HKDF
-    #define WC_RSA_PSS
-    #define WOLFSSL_PSS_LONG_SALT
-#else
-    /* wolfCrypt only (no SSL/TLS) */
-    #define WOLFCRYPT_ONLY
-#endif
+#undef  HAVE_C___ATOMIC
+#define HAVE_C___ATOMIC 1
 
-/* No threading or file system */
+#undef  HAVE_THREAD_LS
+#define HAVE_THREAD_LS
+
+#undef  NO_DO178
+#define NO_DO178
+
+#undef  TFM_NO_ASM
+#define TFM_NO_ASM
+
+#undef  WOLFSSL_NO_ASM
+#define WOLFSSL_NO_ASM
+
+#undef  WOLFSSL_X86_64_BUILD
+#define WOLFSSL_X86_64_BUILD
+
+#undef  SINGLE_THREADED
 #define SINGLE_THREADED
-/* File system disable */
-#if 0
-    #define NO_FILESYSTEM
-#endif
 
-/* Enable crypto callbacks */
-#define WOLF_CRYPTO_CB
+#undef  WC_NO_RNG
+#define WC_NO_RNG
 
-/* Enable PRNG (SHA2-256) */
-#define HAVE_HASHDRBG
+#undef  ERROR_QUEUE_PER_THREAD
+#define ERROR_QUEUE_PER_THREAD
 
-/* Enable SP math all (sp_int.c) with multi-precision support */
-#define WOLFSSL_SP_MATH_ALL
-
-/* Enable hardening (timing resistance) */
+#undef  TFM_TIMING_RESISTANT
 #define TFM_TIMING_RESISTANT
+
+#undef  ECC_TIMING_RESISTANT
 #define ECC_TIMING_RESISTANT
-#define WC_RSA_BLINDING
 
-/* Asymmetric */
-#define HAVE_ECC
-#undef NO_RSA
-#define NO_DH
-#ifndef NO_DH
-    #define HAVE_FFDHE_2048
-    #define HAVE_DH_DEFAULT_PARAMS
-#endif
+#undef  NO_SESSION_CACHE
+#define NO_SESSION_CACHE
 
-/* Symmetric Hash */
-#undef NO_SHA
-#undef NO_SHA256
-#define WOLFSSL_SHA512
-#define WOLFSSL_SHA384
+#undef  RSA_LOW_MEM
+#define RSA_LOW_MEM
 
-/* Symmetric Cipher */
-#define HAVE_AES_KEYWRAP
-#define WOLFSSL_AES_DIRECT
+#undef  GCM_SMALL
+#define GCM_SMALL
+
+#undef  CURVE25519_SMALL
+#define CURVE25519_SMALL
+
+#undef  ED25519_SMALL
+#define ED25519_SMALL
+
+#undef  WOLFSSL_SMALL_CERT_VERIFY
+#define WOLFSSL_SMALL_CERT_VERIFY
+
+#undef  WOLFSSL_NO_ASYNC_IO
+#define WOLFSSL_NO_ASYNC_IO
+
+#undef  USE_SLOW_SHA
+#define USE_SLOW_SHA
+
+#undef  USE_SLOW_SHA256
+#define USE_SLOW_SHA256
+
+#undef  USE_SLOW_SHA512
+#define USE_SLOW_SHA512
+
+#undef  WOLFSSL_AES_CFB
 #define WOLFSSL_AES_CFB
-#define HAVE_AESGCM
-#define GCM_TABLE_4BIT
 
-#if 0
-    #define HAVE_POLY1305
-    #define HAVE_CHACHA
-#endif
+#undef  WOLFSSL_USE_ALIGN
+#define WOLFSSL_USE_ALIGN
 
-/* Features */
-#define WOLFSSL_CERT_GEN
-#define WOLFSSL_CERT_REQ
-#define WOLFSSL_CERT_EXT
+#undef  NO_DSA
+#define NO_DSA
 
-#define HAVE_PKCS7
-#define HAVE_X963_KDF
+#undef  NO_ERROR_STRINGS
+#define NO_ERROR_STRINGS
+
+#undef  NO_OLD_TLS
+#define NO_OLD_TLS
+
+#undef  WOLFSSL_NO_TLS12
+#define WOLFSSL_NO_TLS12
+
+#undef  NO_OLD_TLS
+#define NO_OLD_TLS
+
+#undef  NO_WOLFSSL_MEMORY
+#define NO_WOLFSSL_MEMORY
+
+#undef  NO_RSA
+#define NO_RSA
+
+#undef  NO_DH
+#define NO_DH
+
+#undef  NO_ASN
+#define NO_ASN
+
+#undef  NO_ASN_CRYPT
+#define NO_ASN_CRYPT
+
+#undef  NO_BIG_INT
+#define NO_BIG_INT
+
+#undef  NO_CODING
+#define NO_CODING
+
+#undef  WOLFSSL_BASE64_ENCODE
 #define WOLFSSL_BASE64_ENCODE
 
+#undef  NO_SHA
+#define NO_SHA
 
-/* Disables */
-#define NO_DSA
-#define NO_DES3
-#define NO_RC4
-#define NO_PSK
-#define NO_MD4
+#undef  NO_OLD_TLS
+#define NO_OLD_TLS
+
+#undef  WOLFSSL_NO_SHAKE128
 #define WOLFSSL_NO_SHAKE128
+
+#undef  WOLFSSL_NO_SHAKE256
 #define WOLFSSL_NO_SHAKE256
+
+#undef  NO_CHACHA_ASM
+#define NO_CHACHA_ASM
+
+#undef  WC_NO_HASHDRBG
+#define WC_NO_HASHDRBG
+
+#undef  NO_FILESYSTEM
+#define NO_FILESYSTEM
+
+#undef  NO_INLINE
+#define NO_INLINE
+
+#undef  NO_RC4
+#define NO_RC4
+
+#undef  HAVE_ENCRYPT_THEN_MAC
+#define HAVE_ENCRYPT_THEN_MAC
+
+#undef  NO_PSK
+#define NO_PSK
+
+#undef  NO_MD4
+#define NO_MD4
+
+#undef  NO_PKCS12
+#define NO_PKCS12
+
+#undef  NO_PWDBASED
+#define NO_PWDBASED
+
+#undef  WOLFCRYPT_ONLY
+#define WOLFCRYPT_ONLY
+
+#undef  NO_PKCS8
+#define NO_PKCS8
+
+#undef  WC_NO_ASYNC_THREADING
+#define WC_NO_ASYNC_THREADING
+
+#undef  NO_OLD_RNGNAME
+#define NO_OLD_RNGNAME
+
+#undef  NO_OLD_WC_NAMES
+#define NO_OLD_WC_NAMES
+
+#undef  NO_OLD_SSL_NAMES
+#define NO_OLD_SSL_NAMES
+
+#undef  NO_OLD_SHA_NAMES
+#define NO_OLD_SHA_NAMES
+
+#undef  NO_OLD_MD5_NAME
+#define NO_OLD_MD5_NAME
+
+#undef  HAVE_DH_DEFAULT_PARAMS
+#define HAVE_DH_DEFAULT_PARAMS
+
+#undef  NO_CERTS
+#define NO_CERTS
+
+#undef  NO_ASN
+#define NO_ASN
+
+#undef  NO_MD5
+#define NO_MD5
+
+#undef  NO_OLD_TLS
+#define NO_OLD_TLS
+
+#undef  NO_DES3
+#define NO_DES3
+
+#undef  HAVE___UINT128_T
+#define HAVE___UINT128_T 1
 
 
 #ifdef __cplusplus
