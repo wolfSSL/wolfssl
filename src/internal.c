5aceae1d1c18aec8e9932a3ef8b42de4e6d4a504
Daniel Pouzzner <douzzer@wolfssl.com>
2023-05-17 01:05:14 -0500

    src/internal.c: fix for typo (identicalInnerCondition) in DoClientHello().

--- src/internal.c
+++ src/internal.c
@@ -33326,7 +33326,6 @@ static int DoSessionTicket(WOLFSSL* ssl, const byte* input, word32* inOutIdx,
             ret = DoClientHelloStateless(ssl, input, inOutIdx, helloSz);
             if (ret != 0 || !ssl->options.dtlsStateful) {
                 int alertType = TranslateErrorToAlert(ret);
-                if (alertType != invalid_alert)
                 if (alertType != invalid_alert) {
                     int err;
 
