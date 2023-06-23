59c3c424465add892876dfd43a3f35af6545ba6b
gojimmypi <jim@wolfssl.com>
2023-06-23 08:11:50 -0700

    cyassl settings _POSIX_THREADS HAVE_PTHREAD

--- cyassl/ctaocrypt/settings.h
+++ cyassl/ctaocrypt/settings.h
@@ -31,6 +31,12 @@
     extern "C" {
 #endif
 
+/* pick up compiler def; may need to turn on HAVE_PTHREAD for some configs */
+#ifdef _POSIX_THREADS
+    #undef HAVE_PTHREAD
+    #define HAVE_PTHREAD
+#endif
+
 /* Uncomment next line if using IPHONE */
 /* #define IPHONE */
 
