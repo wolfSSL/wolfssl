fb91f1c1fd5f2b26e1249a978aec7731bc666d18
John Safranek <john@wolfssl.com>
2023-09-05 17:07:08 -0700

    Update the ARM asm files with a missing initialization for SHA2-256 and the AesCtrSetKey function for AES-CTR.

--- wolfcrypt/src/port/arm/armv8-aes.c
+++ wolfcrypt/src/port/arm/armv8-aes.c
@@ -5719,6 +5719,12 @@ int wc_AesCtrEncrypt(Aes* aes, byte* out, const byte* in, word32 sz)
     }
     return 0;
 }
+
+int wc_AesCtrSetKey(Aes* aes, const byte* key, word32 len,
+        const byte* iv, int dir)
+{
+    return wc_AesSetKey(aes, key, len, iv, AES_ENCRYPTION);
+}
 #endif /* WOLFSSL_AES_COUNTER */
 
 #ifdef HAVE_AESCCM
