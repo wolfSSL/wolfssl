f8f080d7ded8b07b45833626dc29dc8c9dc4997f
Andras Fekete <andras@wolfssl.com>
2023-10-23 13:32:33 -0400

    Don't nag about leaked resources

--- examples/asn1/asn1.c
+++ examples/asn1/asn1.c
@@ -470,6 +470,11 @@ int main(int argc, char* argv[])
     if (ret != 0) {
         fprintf(stderr, "%s\n", wc_GetErrorString(ret));
     }
+
+    if (fp != stdin) {
+        fclose(fp);
+        fp = stdin;
+    }
     return (ret == 0) ? 0 : 1;
 }
 
