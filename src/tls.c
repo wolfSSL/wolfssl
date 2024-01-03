e5d8ce9983944ef5c06164b98ecc754de62fd1e7
Andras Fekete <andras@wolfssl.com>
2024-01-03 10:29:56 -0500

    Fix memset size

--- src/tls.c
+++ src/tls.c
@@ -186,7 +186,7 @@ int BuildTlsFinished(WOLFSSL* ssl, Hashes* hashes, const byte* sender)
         return MEMORY_E;
 #endif
 
-    XMEMSET(handshake_hash, 0, sizeof(handshake_hash));
+    XMEMSET(handshake_hash, 0, HSHASH_SZ);
     ret = BuildTlsHandshakeHash(ssl, handshake_hash, &hashSz);
     if (ret == 0) {
         if (XSTRNCMP((const char*)sender, (const char*)kTlsClientStr,
