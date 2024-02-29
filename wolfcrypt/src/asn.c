b3c276bf0b3b7553e6e8344dda4c323d5ade7fdf
jordan <jordan@wolfssl.com>
2024-02-29 13:17:31 -0600

    Fix wc_SetExtKeyUsageOID buffer warning: update size of oid.

--- wolfcrypt/src/asn.c
+++ wolfcrypt/src/asn.c
@@ -31642,7 +31642,7 @@ int wc_SetExtKeyUsage(Cert *cert, const char *value)
 int wc_SetExtKeyUsageOID(Cert *cert, const char *in, word32 sz, byte idx,
         void* heap)
 {
-    byte oid[MAX_OID_SZ];
+    byte oid[CTC_MAX_EKU_OID_SZ];
     word32 oidSz = CTC_MAX_EKU_OID_SZ;
 
     if (idx >= CTC_MAX_EKU_NB || sz >= CTC_MAX_EKU_OID_SZ) {
