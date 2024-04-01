8f599defe0bb03c505e70ac43ee12d0d9ae23afc
Anthony Hu <anthony@wolfssl.com>
2024-04-01 15:20:27 -0400

    Add check inspired by original implementation of asn.

--- wolfcrypt/src/asn.c
+++ wolfcrypt/src/asn.c
@@ -27682,6 +27682,17 @@ static int EncodeName(EncodedName* name, const char* nameStr,
         ret = BAD_FUNC_ARG;
     }
 
+#ifdef WOLFSSL_CUSTOM_OID
+    if (type == ASN_CUSTOM_NAME) {
+        if (cname == NULL || cname->custom.oidSz == 0) {
+            name->used = 0;
+            return 0;
+        }
+    }
+#else
+    (void)cname;
+#endif
+
     CALLOC_ASNSETDATA(dataASN, rdnASN_Length, ret, NULL);
     if (ret == 0) {
         nameSz = (word32)XSTRLEN(nameStr);
