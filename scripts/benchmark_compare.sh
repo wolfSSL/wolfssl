feacac2c404ea7b2226919251936ba1e51622212
Andras Fekete <andras@wolfssl.com>
2024-04-17 11:28:09 -0400

    Clean up script to output color coded data

--- scripts/benchmark_compare.sh
+++ scripts/benchmark_compare.sh
@@ -2,9 +2,12 @@
 
 FIRST_FILE=$1
 SECOND_FILE=$2
+THRESHOLD=${3:-"10"}
 
 declare -A symStats
 declare -A asymStats
+
+mode=0
 while IFS= read -r line; do
     if [[ $line == *"Symmetric Ciphers"* ]]; then
         mode=1
@@ -14,20 +17,41 @@ while IFS= read -r line; do
         mode=2
         read
         read
-    else
+    fi
+    if [ "$mode" = 1 ] || [ "$mode" = 2 ]; then
         if [[ $line == *","* ]]; then
             declare -a fields
             IFS=',' read -ra fields <<< "$line"
+            ALGO=${fields[0]}
+            VALUE=${fields[1]}
 
-            if [ $mode == 1 ]; then
-                symStats["${fields[0]}"]=${fields[1]}
-            elif [ $mode == 2 ]; then
-                asymStats["${fields[0]}"]=${fields[1]}
+            if [ "$mode" = "1" ]; then
+                symStats["${ALGO}"]=${VALUE}
+            elif [ "$mode" = "2" ]; then
+                asymStats["${ALGO}"]=${VALUE}
             fi
         fi
     fi
-done < $FIRST_FILE
+done < ${FIRST_FILE}
 
+RED='\033[0;31m'
+GRN='\033[0;32m'
+NC='\033[0m' # No Color
+function printData() { # printData <ALGO> <val1> <val2>
+    ALGO=$1
+    VAL1=$2
+    VAL2=$3
+    if (( $(echo "sqrt( (${VAL1} - ${VAL2})^2 ) < ${THRESHOLD}" | bc -l) )); then
+        # take absolute value and check if less than a threshold
+        echo "${ALGO},${GRN}${VAL1}${NC},=,${GRN}${VAL2}${NC}\n"
+    elif (( $(echo "${VAL1} > ${VAL2}" | bc -l) )); then
+        echo "${ALGO},${GRN}${VAL1}${NC},>,${VAL2}\n"
+    else
+        echo "${ALGO},${VAL1},<,${GRN}${VAL2}${NC}\n"
+    fi
+}
+
+mode=0
 while IFS= read -r line; do
     if [[ $line == *"Symmetric Ciphers"* ]]; then
         mode=1
@@ -37,26 +61,23 @@ while IFS= read -r line; do
         mode=2
         read
         read
-    else
+    fi
+
+    if [ "$mode" = 1 ] || [ "$mode" = 2 ]; then
         if [[ $line == *","* ]]; then
             declare -a fields
             IFS=',' read -ra fields <<< "$line"
 
-            if [ $mode == 1 ]; then
-                if (( $(echo "${symStats["${fields[0]}"]} > ${fields[1]}" | bc -l) )); then
-                    RES+="${fields[0]}: $FIRST_FILE (${symStats["${fields[0]}"]}) > $SECOND_FILE (${fields[1]})\n"
-                else
-                    RES+="${fields[0]}: $FIRST_FILE (${symStats["${fields[0]}"]}) < $SECOND_FILE (${fields[1]})\n"
-                fi
-            elif [ $mode == 2 ]; then
-                if (( $(echo "${asymStats["${fields[0]}"]} > ${fields[1]}" | bc -l) )); then
-                    RES+="${fields[0]}: $FIRST_FILE (${asymStats["${fields[0]}"]}) > $SECOND_FILE (${fields[1]})\n"
-                else
-                    RES+="${fields[0]}: $FIRST_FILE (${asymStats["${fields[0]}"]}) < $SECOND_FILE (${fields[1]})\n"
-                fi
+            ALGO=${fields[0]}
+            VALUE=${fields[1]}
+
+            if [ "$mode" = "1" ]; then
+                RES+=`printData "${ALGO}" "${symStats["${ALGO}"]}" "${VALUE}"`
+            elif [ "$mode" = "2" ]; then
+                RES+=`printData "${ALGO}" "${asymStats["${ALGO}"]}" "${VALUE}"`
             fi
         fi
     fi
-done < $SECOND_FILE
+done < ${SECOND_FILE}
 
-echo -e "$RES" | column -t
+echo -e "ALGO,${FIRST_FILE},difference,${SECOND_FILE}\n$RES" | column -t -s ','
