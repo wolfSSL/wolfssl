79595a3602c2d3c2f67ac205b4a379882e13b434
JacobBarthelmeh <jacob@wolfssl.com>
2024-05-16 09:48:56 -0600

    account for sp math which defines NO_BIG_INT but still allows mp_int types

--- wolfssl/openssl/bn.h
+++ wolfssl/openssl/bn.h
@@ -40,7 +40,7 @@
 typedef struct WOLFSSL_BIGNUM {
     int neg;        /* openssh deference */
     void *internal; /* our big num */
-#ifndef NO_BIG_INT
+#if !defined(NO_BIG_INT) || defined(WOLFSSL_SP_MATH)
     mp_int mpi;
 #endif
 } WOLFSSL_BIGNUM;
