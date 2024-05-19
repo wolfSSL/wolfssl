d0e73783f1107ca88ba7cd967bea9bf3d8be4620
Daniel Pouzzner <douzzer@wolfssl.com>
2024-05-18 22:00:00 -0500

    wolfcrypt/src/aes.c and wolfssl/wolfcrypt/aes.h: add FIPS_AES_XTS_MAX_BYTES_PER_TWEAK and struct XtsAesStreamData, with improved error checking on streaming AES-XTS APIs;
    wolfcrypt/test/test.c and linuxkm/lkcapi_glue.c: update AES-XTS streaming calls to use struct XtsAesStreamData;
    
    linuxkm/lkcapi_glue.c: add handling for CONFIG_CRYPTO_MANAGER*.

--- wolfssl/wolfcrypt/aes.h
+++ wolfssl/wolfcrypt/aes.h
@@ -405,6 +405,13 @@ struct Aes {
 #endif
 
 #ifdef WOLFSSL_AES_XTS
+    #if FIPS_VERSION3_GE(6,0,0)
+        /* SP800-38E - Restrict data unit to 2^20 blocks per key. A block is
+         * AES_BLOCK_SIZE or 16-bytes (128-bits). So each key may only be used to
+         * protect up to 1,048,576 blocks of AES_BLOCK_SIZE (16,777,216 bytes)
+         */
+        #define FIPS_AES_XTS_MAX_BYTES_PER_TWEAK 16777216
+    #endif
     struct XtsAes {
         Aes aes;
     #ifdef WC_AES_XTS_SUPPORT_SIMULTANEOUS_ENC_AND_DEC_KEYS
@@ -417,6 +424,14 @@ struct Aes {
         typedef struct XtsAes XtsAes;
         #define WC_AESXTS_TYPE_DEFINED
     #endif
+
+    #ifdef WOLFSSL_AESXTS_STREAM
+        struct XtsAesStreamData {
+            byte tweak_block[AES_BLOCK_SIZE];
+            word32 bytes_crypted_with_this_tweak;
+        };
+    #endif
+
 #endif
 
 
@@ -671,21 +686,23 @@ WOLFSSL_API int wc_AesXtsDecryptConsecutiveSectors(XtsAes* aes,
 
 #ifdef WOLFSSL_AESXTS_STREAM
 
-WOLFSSL_API int wc_AesXtsEncryptInit(XtsAes* aes, byte* i, word32 iSz);
+WOLFSSL_API int wc_AesXtsEncryptInit(XtsAes* aes, const byte* i, word32 iSz,
+         struct XtsAesStreamData *stream);
 
-WOLFSSL_API int wc_AesXtsDecryptInit(XtsAes* aes, byte* i, word32 iSz);
+WOLFSSL_API int wc_AesXtsDecryptInit(XtsAes* aes, const byte* i, word32 iSz,
+         struct XtsAesStreamData *stream);
 
 WOLFSSL_API int wc_AesXtsEncryptUpdate(XtsAes* aes, byte* out,
-         const byte* in, word32 sz, byte *i);
+         const byte* in, word32 sz, struct XtsAesStreamData *stream);
 
 WOLFSSL_API int wc_AesXtsDecryptUpdate(XtsAes* aes, byte* out,
-         const byte* in, word32 sz, byte *i);
+         const byte* in, word32 sz, struct XtsAesStreamData *stream);
 
 WOLFSSL_API int wc_AesXtsEncryptFinal(XtsAes* aes, byte* out,
-         const byte* in, word32 sz, byte *i);
+         const byte* in, word32 sz, struct XtsAesStreamData *stream);
 
 WOLFSSL_API int wc_AesXtsDecryptFinal(XtsAes* aes, byte* out,
-         const byte* in, word32 sz, byte *i);
+         const byte* in, word32 sz, struct XtsAesStreamData *stream);
 
 #endif /* WOLFSSL_AESXTS_STREAM */
 
