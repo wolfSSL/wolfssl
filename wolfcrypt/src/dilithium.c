fc19c36bf801330b2393e3edaf1546e1dcd9e6df
Sean Parkinson <sean@wolfssl.com>
2024-07-29 10:48:08 +1000

    Dilithium: fix check hint
    When all indeces are 0, then don't check hints against indeces.

--- wolfcrypt/src/dilithium.c
+++ wolfcrypt/src/dilithium.c
@@ -3183,11 +3183,11 @@ static int dilithium_check_hint(const byte* h, byte k, byte omega)
     unsigned int i;
 
     /* Skip polynomial index while count is 0. */
-    while ((h[omega + o] == 0) && (o < k)) {
+    while ((o < k) && (h[omega + o] == 0)) {
         o++;
     }
     /* Check all possible hints. */
-    for (i = 1; i < omega; i++) {
+    for (i = 1; (o < k) && (i < omega); i++) {
         /* Done with polynomial if index equals count of hints. */
         if (i == h[omega + o]) {
             /* Next polynomial index while count is index. */
