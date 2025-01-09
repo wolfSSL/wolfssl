aa8a2144c838ded86a2c58e47d34a9e30febddaf
Sean Parkinson <sean@wolfssl.com>
2025-01-10 08:28:45 +1000

    Aarch64 CPU Id: FreeBSD/OpenBSD fix
    Fix name and flags set.

--- wolfcrypt/src/cpuid.c
+++ wolfcrypt/src/cpuid.c
@@ -259,8 +259,10 @@
 
             if (features & CPUID_AARCH64_FEAT_AES)
                 cpuid_flags |= CPUID_AES;
-            if (features & CPUID_AARCH64_FEAT_PMULL)
+            if (features & CPUID_AARCH64_FEAT_AES_PMULL) {
+                cpuid_flags |= CPUID_AES;
                 cpuid_flags |= CPUID_PMULL;
+            }
             if (features & CPUID_AARCH64_FEAT_SHA256)
                 cpuid_flags |= CPUID_SHA256;
             if (features & CPUID_AARCH64_FEAT_SHA256_512)
