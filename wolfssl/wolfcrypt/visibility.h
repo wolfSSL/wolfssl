47d130440dea1f87622042de0b7aa4e09a14f295
Jiri Malak <malak.jiri@gmail.com>
2025-02-26 10:26:28 +0100

    remove now useless __WATCOMC__ macro check

--- wolfssl/wolfcrypt/visibility.h
+++ wolfssl/wolfcrypt/visibility.h
@@ -59,7 +59,7 @@
         #endif
         #define WOLFSSL_LOCAL
     #elif defined(_MSC_VER) || defined(__MINGW32__) || defined(__CYGWIN__) || \
-        defined(_WIN32_WCE) || defined(__WATCOMC__)
+        defined(_WIN32_WCE)
         #if defined(WOLFSSL_DLL)
             #define WOLFSSL_API __declspec(dllimport)
         #else
