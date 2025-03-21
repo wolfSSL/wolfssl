a57326d50018c954c1a327d4c568c852e6a6f976
Daniel Pouzzner <douzzer@wolfssl.com>
2025-03-21 16:56:48 -0500

    fix whitespace in tests/api/test_evp.c.

--- tests/api/test_evp.c
+++ tests/api/test_evp.c
@@ -74,7 +74,7 @@ int test_wolfSSL_EVP_CIPHER_type_string(void)
     EXPECT_DECLS;
 #ifdef OPENSSL_EXTRA
     const char* cipherStr;
-    
+
     /* Test with valid cipher types */
 #ifndef NO_AES
     #ifdef WOLFSSL_AES_128
@@ -94,7 +94,7 @@ int test_wolfSSL_EVP_CIPHER_type_string(void)
     cipherStr = wolfSSL_EVP_CIPHER_type_string(WC_NULL_CIPHER_TYPE);
     ExpectNotNull(cipherStr);
     ExpectStrEQ(cipherStr, "NULL");
-    
+
     /* Test with invalid cipher type */
     cipherStr = wolfSSL_EVP_CIPHER_type_string(0xFFFF);
     ExpectNull(cipherStr);
