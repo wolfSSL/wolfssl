f609d423d730d1dddd07a3c260ed271a53379782
Daniel Pouzzner <douzzer@wolfssl.com>
2025-04-10 17:23:17 +0000

    linuxkm/Kbuild: treat KERNEL_ARCH "x86_64" as "x86" and remove inapt -mpreferred-stack-boundary=4 from x86 WOLFSSL_CFLAGS;
    linuxkm/linuxkm_wc_port.h: use >=6.9.0 as the gate for 5-arg fortify_panic();
    
    in lkm_printf() definition, use _printk on >5.15.0;
    
    linuxkm/module_hooks.c: raise MAX_FIPS_DATA_SZ and MAX_FIPS_CODE_SZ to
    accommodate growth.

--- linuxkm/linuxkm_wc_port.h
+++ linuxkm/linuxkm_wc_port.h
@@ -137,7 +137,7 @@
              * fortify_panic().
              */
             extern void __my_fortify_panic(const char *name) __noreturn __cold;
-            #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,8,0)
+            #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0)
                 /* see linux 3d965b33e40d9 */
                 #define fortify_panic(func, write, avail, size, retfail) \
                         __my_fortify_panic(#func)
@@ -882,7 +882,11 @@
      */
     #define key_update wc_key_update
 
-    #define lkm_printf(format, args...) printk(KERN_INFO "wolfssl: %s(): " format, __func__, ## args)
+    #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+        #define lkm_printf(format, args...) _printk(KERN_INFO "wolfssl: %s(): " format, __func__, ## args)
+    #else
+        #define lkm_printf(format, args...) printk(KERN_INFO "wolfssl: %s(): " format, __func__, ## args)
+    #endif
     #define printf(...) lkm_printf(__VA_ARGS__)
 
     #ifdef HAVE_FIPS
