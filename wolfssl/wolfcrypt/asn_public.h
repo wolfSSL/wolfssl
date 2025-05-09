79f214f73c28c9b56c13f9f6cf498aa2091846d2
Brett Nicholas <7547222+bigbrett@users.noreply.github.com>
2025-05-09 11:29:19 -0600

    add new X509 API: wc_Exportx509PubKeyWithSpki

--- wolfssl/wolfcrypt/asn_public.h
+++ wolfssl/wolfcrypt/asn_public.h
@@ -879,6 +879,8 @@ WOLFSSL_API int  wc_ParseCert(
 
 WOLFSSL_API int wc_GetPubKeyDerFromCert(struct DecodedCert* cert,
                                         byte* derKey, word32* derKeySz);
+WOLFSSL_API int wc_ExportX509PubKeyWithSpki(const byte* cert, word32 certSz,
+                                            byte* pubKey, word32* pubKeySz);
 
 #ifdef WOLFSSL_FPKI
 WOLFSSL_API int wc_GetUUIDFromCert(struct DecodedCert* cert,
