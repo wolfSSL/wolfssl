fc1d2812685e6d1140bf77ba8f823c201b869f46
Sean Parkinson <sean@wolfssl.com>
2025-05-20 10:55:26 +1000

    Green Hills compiler fixes
    internal.c: Move non-enumeration value out of switch.
    ssl.c: Only declare globalRNGMutex when required.
    x509.c: initialize ret
    
    armv8-aes.c, armv8-chacha.c: fix branch instructions
    armv8-mlkem*: ensure only required constants are input operands and move
    constants closer to first use.
    armv8-poly1305.c: remove POLY1305_BLOCK_SIZE from input operands.
    armv8-sha3-asm_c.c, armv8-sha512-asm_c.c: use constraint ':' instead of
    'S'.
    armv8-sha512.c: initialize initfp. Is always used.

--- wolfcrypt/src/port/arm/armv8-sha512-asm_c.c
+++ wolfcrypt/src/port/arm/armv8-sha512-asm_c.c
@@ -1004,8 +1004,8 @@ void Transform_Sha512_Len_neon(wc_Sha512* sha512, const byte* data, word32 len)
         "stp	x8, x9, [%x[sha512], #32]\n\t"
         "stp	x10, x11, [%x[sha512], #48]\n\t"
         : [sha512] "+r" (sha512), [data] "+r" (data), [len] "+r" (len)
-        : [L_SHA512_transform_neon_len_k] "S" (L_SHA512_transform_neon_len_k),
-          [L_SHA512_transform_neon_len_r8] "S" (L_SHA512_transform_neon_len_r8)
+        : [L_SHA512_transform_neon_len_k] "i" (L_SHA512_transform_neon_len_k),
+          [L_SHA512_transform_neon_len_r8] "i" (L_SHA512_transform_neon_len_r8)
         : "memory", "cc", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10",
             "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x19", "x20",
             "x21", "x22", "x23", "x24", "x25", "x26", "x27", "v0", "v1", "v2",
@@ -1580,7 +1580,7 @@ void Transform_Sha512_Len_crypto(wc_Sha512* sha512, const byte* data,
         /* Store digest back */
         "st1	{v24.2d, v25.2d, v26.2d, v27.2d}, [%x[sha512]]\n\t"
         : [sha512] "+r" (sha512), [data] "+r" (data), [len] "+r" (len)
-        : [L_SHA512_trans_crypto_len_k] "S" (L_SHA512_trans_crypto_len_k)
+        : [L_SHA512_trans_crypto_len_k] "i" (L_SHA512_trans_crypto_len_k)
         : "memory", "cc", "x3", "x4", "v0", "v1", "v2", "v3", "v4", "v5", "v6",
             "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16",
             "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25",
