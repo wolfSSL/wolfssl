81ead6e97e99845b2079ddaa719b80de92541512
Anthony Hu <anthony@wolfssl.com>
2025-06-18 18:11:46 -0400

    Add a tests for the RSA APIs

--- tests/api.c
+++ tests/api.c
@@ -5062,6 +5062,96 @@ static int test_wolfSSL_CertRsaPss(void)
 }
 #endif
 
+
+/* Test RSA-PSS digital signature creation and verification */
+static int test_wc_RsaPSS_DigitalSignVerify(void)
+{
+    EXPECT_DECLS;
+
+#if !defined(NO_RSA) && defined(OPENSSL_EXTRA) && defined(WOLFSSL_KEY_GEN)
+    /* Test digest */
+    const unsigned char test_digest[32] = {
+        0x08, 0x09, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+        0x06, 0x07, 0x08, 0x09, 0x00, 0x01, 0x02, 0x03,
+        0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x01,
+        0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09
+    };
+    const unsigned int digest_len = 32;
+
+    /* Variables for RSA key generation and signature operations */
+    EVP_PKEY_CTX *key_ctx = NULL;
+    EVP_PKEY *pkey = NULL;
+    EVP_PKEY_CTX *sign_ctx = NULL;
+    EVP_PKEY_CTX *verify_ctx = NULL;
+    unsigned char signature[384] = {0};
+    size_t signature_len = sizeof(signature);
+    int modulus_bits = 2048;
+
+    /* Generate RSA key pair to avoid file dependencies */
+    ExpectNotNull(key_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL));
+    ExpectIntEQ(EVP_PKEY_keygen_init(key_ctx), 1);
+    ExpectIntEQ(EVP_PKEY_CTX_set_rsa_keygen_bits(key_ctx, modulus_bits), 1);
+    ExpectIntEQ(EVP_PKEY_keygen(key_ctx, &pkey), 1);
+
+    /* Create signing context */
+    ExpectNotNull(sign_ctx = EVP_PKEY_CTX_new(pkey, NULL));
+    ExpectIntEQ(EVP_PKEY_sign_init(sign_ctx), 1);
+
+    /* Configure RSA-PSS parameters for signing */
+    ExpectIntEQ(EVP_PKEY_CTX_set_rsa_padding(sign_ctx, RSA_PKCS1_PSS_PADDING),
+                1);
+    ExpectIntGT(EVP_PKEY_CTX_set_rsa_pss_saltlen(sign_ctx, 32), 0);
+    ExpectIntGT(EVP_PKEY_CTX_set_rsa_mgf1_md(sign_ctx, EVP_sha256()), 0);
+    ExpectIntEQ(EVP_PKEY_CTX_set_signature_md(sign_ctx, EVP_sha256()), 1);
+
+    /* Create the digital signature */
+    ExpectIntEQ(EVP_PKEY_sign(sign_ctx, signature, &signature_len, test_digest,
+                              digest_len), 1);
+    ExpectIntGT((int)signature_len, 0);
+
+    /* Create verification context */
+    ExpectNotNull(verify_ctx = EVP_PKEY_CTX_new(pkey, NULL));
+    ExpectIntEQ(EVP_PKEY_verify_init(verify_ctx), 1);
+
+    /* Configure RSA-PSS parameters for verification */
+    ExpectIntEQ(EVP_PKEY_CTX_set_rsa_padding(verify_ctx, RSA_PKCS1_PSS_PADDING),
+                1);
+
+    ExpectIntGT(EVP_PKEY_CTX_set_rsa_pss_saltlen(verify_ctx, 32), 0);
+    ExpectIntGT(EVP_PKEY_CTX_set_rsa_mgf1_md(verify_ctx, EVP_sha256()), 0);
+    ExpectIntEQ(EVP_PKEY_CTX_set_signature_md(verify_ctx, EVP_sha256()), 1);
+
+    /* Verify the digital signature */
+    ExpectIntEQ(EVP_PKEY_verify(verify_ctx, signature, signature_len,
+                                test_digest, digest_len), 1);
+
+    /* Test with wrong digest to ensure verification fails (negative test) */
+    {
+        const unsigned char wrong_digest[32] = {
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            0x09, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+            0x07, 0x08, 0x09, 0x00, 0x01, 0x02, 0x03, 0x04,
+            0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x01, 0x02
+        };
+        ExpectIntNE(EVP_PKEY_verify(verify_ctx, signature, signature_len,
+                    wrong_digest, digest_len), 1);
+    }
+
+    /* Clean up */
+    if (verify_ctx)
+        EVP_PKEY_CTX_free(verify_ctx);
+    if (sign_ctx)
+        EVP_PKEY_CTX_free(sign_ctx);
+    if (pkey)
+        EVP_PKEY_free(pkey);
+    if (key_ctx)
+        EVP_PKEY_CTX_free(key_ctx);
+
+#endif /* !NO_RSA && OPENSSL_EXTRA && WOLFSSL_KEY_GEN */
+
+    return EXPECT_RESULT();
+}
+
 static int test_wolfSSL_CTX_load_verify_locations_ex(void)
 {
     EXPECT_DECLS;
@@ -67886,6 +67976,7 @@ TEST_CASE testCases[] = {
 #ifdef HAVE_CERT_CHAIN_VALIDATION
     TEST_DECL(test_various_pathlen_chains),
 #endif
+TEST_DECL(test_wc_RsaPSS_DigitalSignVerify),
 
     /*********************************
      * SSL/TLS API tests
