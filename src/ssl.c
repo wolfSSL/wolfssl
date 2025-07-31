a652b733e47cee7a3e2dea7a14070f0723f398ec
Kareem <kareem@wolfssl.com>
2025-07-30 17:37:32 -0700

    Fix conversion warning.

--- src/ssl.c
+++ src/ssl.c
@@ -6173,7 +6173,7 @@ int SetCAType(WOLFSSL_CERT_MANAGER* cm, byte* hash, int type)
     #endif
 
         if (XMEMCMP(hash, subjectHash, SIGNER_DIGEST_SIZE) == 0) {
-            current->type = type;
+            current->type = (byte)type;
             ret = WOLFSSL_SUCCESS;
             break;
         }
