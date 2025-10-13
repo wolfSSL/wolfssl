bae25afa40f9c0abbef68302c601a5b1bbe6224c
Eric Blankenhorn <eric@wolfssl.com>
2025-10-13 12:42:01 -0500

    Fix from testing

--- wolfcrypt/src/evp.c
+++ wolfcrypt/src/evp.c
@@ -7012,7 +7012,7 @@ void wolfSSL_EVP_init(void)
     }
 
     static int EvpCipherAesCCM(WOLFSSL_EVP_CIPHER_CTX* ctx, byte* dst,
-                               byte* src, word32 len)
+                               const byte* src, word32 len)
     {
         int ret = WC_NO_ERR_TRACE(WOLFSSL_FAILURE);
 
