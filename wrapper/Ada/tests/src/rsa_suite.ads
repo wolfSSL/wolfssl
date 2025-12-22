0f98e3f21bf4d90a5b3e0ef8fdd5962cedeb3e9d
Juliusz Sosinowicz <juliusz@wolfssl.com>
2025-12-22 22:49:17 +0100

    Add AUnit RSA sign/verify tests and clean up Ada tests warnings

---
+++ wrapper/Ada/tests/src/rsa_suite.ads
@@ -0,0 +1,6 @@
+with AUnit.Test_Suites;
+
+package RSA_Suite is
+   --  Aggregate suite for RSA binding tests.
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+end RSA_Suite;
--- wrapper/Ada/tests/src/tests.adb
+++ wrapper/Ada/tests/src/tests.adb
@@ -1,12 +1,25 @@
 with AUnit.Reporter.Text;
 with AUnit.Run;
+with AUnit.Test_Suites;
 
 --  Minimal AUnit runner.
 --  AUnit uses a generic test runner instantiated with your suite function.
 with SHA256_Suite;
+with RSA_Suite;
 
 procedure Tests is
-   procedure Runner is new AUnit.Run.Test_Runner (SHA256_Suite.Suite);
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
+   function Suite return AUnit.Test_Suites.Access_Test_Suite is
+      S : constant AUnit.Test_Suites.Access_Test_Suite :=
+        new AUnit.Test_Suites.Test_Suite;
+   begin
+      AUnit.Test_Suites.Add_Test (S, SHA256_Suite.Suite);
+      AUnit.Test_Suites.Add_Test (S, RSA_Suite.Suite);
+      return S;
+   end Suite;
+
+   procedure Runner is new AUnit.Run.Test_Runner (Suite);
 
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
