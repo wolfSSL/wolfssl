99c089411c55b542450fcf7a6f571a82884a5b76
David Garske <david@wolfssl.com>
2026-03-23 08:10:03 -0700

    Fix memory leak after refactor

--- wolfcrypt/src/asn.c
+++ wolfcrypt/src/asn.c
@@ -33617,6 +33617,9 @@ void FreeDecodedCRL(DecodedCRL* dcrl)
 
     while(tmp) {
         RevokedCert* next = tmp->next;
+#if defined(OPENSSL_EXTRA)
+        XFREE(tmp->extensions, dcrl->heap, DYNAMIC_TYPE_REVOKED);
+#endif
         XFREE(tmp, dcrl->heap, DYNAMIC_TYPE_REVOKED);
         tmp = next;
     }
