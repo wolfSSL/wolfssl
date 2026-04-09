33310010a9a6b1172c2be2f6941933a54f043dfd
Eric Blankenhorn <eric@wolfssl.com>
2026-04-09 14:41:36 -0500

    Fix wolfSSL_sk_X509_OBJECT_deep_copy to check bounds

--- tests/api/test_x509.h
+++ tests/api/test_x509.h
@@ -28,6 +28,8 @@ int test_x509_set_serialNumber(void);
 int test_x509_verify_cert_hostname_check(void);
 int test_x509_time_field_overread_via_tls(void);
 int test_x509_CertFromX509_akid_overflow(void);
+int test_x509_ReqCertFromX509_skid_overflow(void);
+int test_x509_ReqCertFromX509_skid_boundary(void);
 
 #define TEST_X509_DECLS                                                        \
     TEST_DECL_GROUP("x509", test_x509_rfc2818_verification_callback),          \
@@ -35,6 +37,8 @@ int test_x509_CertFromX509_akid_overflow(void);
     TEST_DECL_GROUP("x509", test_x509_set_serialNumber),                       \
     TEST_DECL_GROUP("x509", test_x509_verify_cert_hostname_check),             \
     TEST_DECL_GROUP("x509", test_x509_time_field_overread_via_tls),             \
-    TEST_DECL_GROUP("x509", test_x509_CertFromX509_akid_overflow)
+    TEST_DECL_GROUP("x509", test_x509_CertFromX509_akid_overflow),             \
+    TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_skid_overflow),          \
+    TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_skid_boundary)
 
 #endif /* WOLFCRYPT_TEST_X509_H */
