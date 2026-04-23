8ddb7a47ad0a26465d47657175b0b1168369180a
Jeremiah Mackey <jeremiahmackey91@gmail.com>
2026-04-23 16:18:33 +0000

    add PQ key integrity tests

--- tests/api/test_slhdsa.c
+++ tests/api/test_slhdsa.c
@@ -1164,6 +1164,10 @@ int test_wc_slhdsa_check_key(void)
     ExpectIntEQ(wc_SlhDsaKey_ImportPublic(&key, pubKey, pubKeyLen), 0);
     ExpectIntEQ(wc_SlhDsaKey_ImportPrivate(&key, privKey, privKeyLen), 0);
     ExpectIntEQ(wc_SlhDsaKey_CheckKey(&key), 0);
+
+    key.sk[0] ^= 0x01;
+    ExpectIntEQ(wc_SlhDsaKey_CheckKey(&key),
+        WC_NO_ERR_TRACE(WC_KEY_MISMATCH_E));
     wc_SlhDsaKey_Free(&key);
 
     wc_FreeRng(&rng);
