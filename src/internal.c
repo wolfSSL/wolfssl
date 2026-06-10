b04601479ca88a6245c2dbdced1e1764cd9cef71
Colton Willey <colton@wolfssl.com>
2026-06-10 11:26:44 -0700

    Remove leftover line re-wrap in SendCertificateStatus

--- src/internal.c
+++ src/internal.c
@@ -26408,8 +26408,7 @@ int SendCertificateStatus(WOLFSSL* ssl)
                 }
 
                 if (chain && chain->buffer) {
-                    while (ret == 0 &&
-                           idx + OPAQUE24_LEN < chain->length) {
+                    while (ret == 0 && idx + OPAQUE24_LEN < chain->length) {
                         c24to32(chain->buffer + idx, &der.length);
                         idx += OPAQUE24_LEN;
 
