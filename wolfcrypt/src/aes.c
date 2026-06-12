7064eeca8907a967817389f1223d0fc88a1b88e3
Josh Holtrop <josh@wolfssl.com>
2026-06-12 12:19:06 -0400

    Force-zero output buffer for AES-SIV decrypt for other AES errors as well

--- wolfcrypt/src/aes.c
+++ wolfcrypt/src/aes.c
@@ -17098,10 +17098,10 @@ static WARN_UNUSED_RESULT int AesSivCipher(
                 WOLFSSL_MSG("Computed SIV doesn't match received SIV.");
                 ret = AES_SIV_AUTH_E;
             }
+        }
 
-            if (ret != 0) {
-                ForceZero(out, dataSz);
-            }
+        if (ret != 0) {
+            ForceZero(out, dataSz);
         }
 
     #ifdef WOLFSSL_SMALL_STACK
