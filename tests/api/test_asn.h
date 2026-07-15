1a3c807e34c986b3d36356982dc38db6b76ad378
Aidan Keefe <aidan.keefe@wolfssl.com>
2026-07-15 11:47:38 -0600

    hardening of OID Encoding

--- tests/api/test_asn.h
+++ tests/api/test_asn.h
@@ -82,18 +82,13 @@ int test_wc_AsnFeatureCoverage(void);
     TEST_DECL_GROUP("asn", test_DecodeCertExtensions_empty_certpol_trailing), \
     TEST_DECL_GROUP("asn", test_ParseCert_validity_length_overrun), \
     TEST_DECL_GROUP("asn", test_ParseCert_SM3wSM2_short_pubkey),    \
-<<<<<<< HEAD
     TEST_DECL_GROUP("asn", test_ParseCert_dnBufferBoundary),        \
     TEST_DECL_GROUP("asn", test_ParseCert_nameComponentIds),       \
     TEST_DECL_GROUP("asn", test_ParseCert_issuerNameNoField),      \
     TEST_DECL_GROUP("asn", test_wc_DecodeObjectId),                 \
-||||||| parent of a5d67d1ce (Updated size of OID represention to be word32 instead of word16 so OIDs)
-    TEST_DECL_GROUP("asn", test_wc_DecodeObjectId),                 \
-=======
     TEST_DECL_GROUP("asn", test_wc_DecodeObjectId_ex),              \
     TEST_DECL_GROUP("asn", test_wc_EncodeObjectId),                 \
     TEST_DECL_GROUP("asn", test_wc_DecodeObjectId_FIPS16),          \
->>>>>>> a5d67d1ce (Updated size of OID represention to be word32 instead of word16 so OIDs)
     TEST_DECL_GROUP("asn", test_ToTraditional_ex_handcrafted),      \
     TEST_DECL_GROUP("asn", test_ToTraditional_ex_roundtrip),        \
     TEST_DECL_GROUP("asn", test_ToTraditional_ex_negative),         \
