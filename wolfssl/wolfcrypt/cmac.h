dceb59177ae770c30b5de9a78ff437d494622d10
Lealem Amedie <lealemta47@gmail.com>
2026-07-26 09:29:42 -0600

    Limit WC_CMAC_TAG_MIN_SZ bump to FIPS builds

--- wolfssl/wolfcrypt/cmac.h
+++ wolfssl/wolfcrypt/cmac.h
@@ -99,12 +99,14 @@ struct Cmac {
 
 
 /* Reasonable defaults */
-/* SP800-38b recommends a minimum tag length of 64-bits */
 #ifndef WC_CMAC_TAG_MAX_SZ
     #define WC_CMAC_TAG_MAX_SZ 16
 #endif
-#ifndef WC_CMAC_TAG_MIN_SZ
+/* SP800-38b recommends a minimum tag length of 64-bits */
+#if FIPS_VERSION3_GE(7,0,0)
     #define WC_CMAC_TAG_MIN_SZ 8
+#elif !defined(WC_CMAC_TAG_MIN_SZ)
+    #define WC_CMAC_TAG_MIN_SZ 4
 #endif
 
 #if FIPS_VERSION3_GE(6,0,0)
