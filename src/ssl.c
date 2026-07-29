4fd5ff63fcbecd1a291817c0e47ea850ee237409
Juliusz Sosinowicz <juliusz@wolfssl.com>
2026-07-29 14:41:23 +0000

    Don't use __DATE__/__TIME__ in kernel module builds
    The Linux kernel build compiles with -Werror=date-time, so OPENSSL_BUILT_ON
    broke libwolfssl.ko.

--- src/ssl.c
+++ src/ssl.c
@@ -6067,7 +6067,8 @@ const char* wolfSSL_OpenSSL_version(int type)
         case OPENSSL_CFLAGS:
             return "compiler: information not available";
         case OPENSSL_BUILT_ON:
-#ifdef HAVE_REPRODUCIBLE_BUILD
+        /* Kernel module builds compile with -Werror=date-time. */
+#if defined(HAVE_REPRODUCIBLE_BUILD) || defined(WOLFSSL_LINUXKM)
             return "built on: date not available";
 #else
             return "built on: " __DATE__ " " __TIME__;
