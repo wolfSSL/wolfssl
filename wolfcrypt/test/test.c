8f5137c08a883294d3942a7d43364651a0bb63ba
night1rider <backman23@live.com>
2026-07-29 16:13:46 -0600

    Xilinx Versal Gen2 ASU port: AES ciphers, CMAC, GMAC offload

--- wolfcrypt/test/test.c
+++ wolfcrypt/test/test.c
@@ -15508,7 +15508,8 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
           ctrPlain, (int)sizeof(oddCipher), ctr128Cipher },
         /* and an additional 9 bytes to reuse tmp left buffer */
         { NULL, 0, NULL, ctrPlain, (int)sizeof(oddCipher), oddCipher },
-        /* Counter wrapping */
+        /* NO_AES_CTR_WRAP_TEST disables testing counter wrap on the low 32 bits. */
+    #ifndef NO_AES_CTR_WRAP_TEST
     #ifndef WOLFSSL_NXP_HASHCRYPT_AES
         { ctr128Key, (int)sizeof(ctr128Key), ctrIvWrap128,
           ctrPlain, (int)sizeof(ctr128Wrap128Cipher), ctr128Wrap128Cipher },
@@ -15545,6 +15546,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
           ctrPlain, (int)sizeof(ctr128Wrap32_2CipherLong),
           ctr128Wrap32_2CipherLong },
     #endif
+    #endif /* !NO_AES_CTR_WRAP_TEST */
 #endif
 #ifdef WOLFSSL_AES_192
         { ctr192Key, (int)sizeof(ctr192Key), ctrIv,
@@ -15552,7 +15554,8 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
         /* let's try with just 9 bytes, non block size test */
         { ctr192Key, (int)sizeof(ctr192Key), ctrIv,
           ctrPlain, (int)sizeof(oddCipher), ctr192Cipher },
-        /* Counter wrapping */
+        /* NO_AES_CTR_WRAP_TEST disables testing counter wrap on the low 32 bits. */
+    #ifndef NO_AES_CTR_WRAP_TEST
     #ifndef WOLFSSL_NXP_HASHCRYPT_AES
         { ctr192Key, (int)sizeof(ctr192Key), ctrIvWrap128,
           ctrPlain, (int)sizeof(ctr192Wrap128Cipher), ctr192Wrap128Cipher },
@@ -15589,6 +15592,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
           ctrPlain, (int)sizeof(ctr192Wrap32_2CipherLong),
           ctr192Wrap32_2CipherLong },
     #endif
+    #endif /* !NO_AES_CTR_WRAP_TEST */
 #endif
 #ifdef WOLFSSL_AES_256
         { ctr256Key, (int)sizeof(ctr256Key), ctrIv,
@@ -15596,7 +15600,8 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
         /* let's try with just 9 bytes, non block size test */
         { ctr256Key, (int)sizeof(ctr256Key), ctrIv,
           ctrPlain, (int)sizeof(oddCipher), ctr256Cipher },
-        /* Counter wrapping */
+        /* NO_AES_CTR_WRAP_TEST disables testing counter wrap on the low 32 bits. */
+    #ifndef NO_AES_CTR_WRAP_TEST
     #ifndef WOLFSSL_NXP_HASHCRYPT_AES
         { ctr256Key, (int)sizeof(ctr256Key), ctrIvWrap128,
           ctrPlain, (int)sizeof(ctr256Wrap128Cipher), ctr256Wrap128Cipher },
@@ -15633,6 +15638,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t aes_ctr_test(void)
           ctrPlain, (int)sizeof(ctr256Wrap32_2CipherLong),
           ctr256Wrap32_2CipherLong },
     #endif
+    #endif /* !NO_AES_CTR_WRAP_TEST */
 #endif
     };
     #define AES_CTR_TEST_LEN (int)(sizeof(testVec) / sizeof(*testVec))
