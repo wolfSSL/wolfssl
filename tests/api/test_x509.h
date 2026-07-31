9e5c365cd1c4fb006aead36fdd46a774a3fa587b
Chris Conlon <chris@wolfssl.com>
2026-07-31 16:00:17 -0600

    Encode basicConstraints critical flag and pathlen into generated CSRs

--- tests/api/test_x509.h
+++ tests/api/test_x509.h
@@ -30,6 +30,7 @@ int test_x509_time_field_overread_via_tls(void);
 int test_x509_CertFromX509_akid_overflow(void);
 int test_x509_ReqCertFromX509_skid_overflow(void);
 int test_x509_ReqCertFromX509_skid_boundary(void);
+int test_x509_ReqCertFromX509_ext_critical(void);
 
 #define TEST_X509_DECLS                                                        \
     TEST_DECL_GROUP("x509", test_x509_rfc2818_verification_callback),          \
@@ -39,6 +40,7 @@ int test_x509_ReqCertFromX509_skid_boundary(void);
     TEST_DECL_GROUP("x509", test_x509_time_field_overread_via_tls),             \
     TEST_DECL_GROUP("x509", test_x509_CertFromX509_akid_overflow),             \
     TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_skid_overflow),          \
-    TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_skid_boundary)
+    TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_skid_boundary),          \
+    TEST_DECL_GROUP("x509", test_x509_ReqCertFromX509_ext_critical)
 
 #endif /* WOLFCRYPT_TEST_X509_H */
