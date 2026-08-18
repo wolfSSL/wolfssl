e1a14d9b1a701e797ab437d988be52087dc97a29
kaleb-himes <kaleb@wolfssl.com>
2026-08-18 11:30:02 -0600

    Guard the whitebox TUs on symbol availability, not HAVE_FIPS

--- tests/unit-mcdc/test_sha256_whitebox.c
+++ tests/unit-mcdc/test_sha256_whitebox.c
@@ -88,7 +88,8 @@ static int wb_fail = 0;
  * variants) there is nothing to force, so the supplement is a no-op. */
 #if !defined(NO_SHA256) && defined(WOLFSSL_X86_64_BUILD) && \
     defined(USE_INTEL_SPEEDUP) && \
-    (!defined(WC_C_DYNAMIC_FALLBACK) || defined(HAVE_FIPS)) && \
+    !(defined(WC_C_DYNAMIC_FALLBACK) && \
+      defined(WC_ALLOW_RUNTIME_IMPL_SELECT)) && \
     (defined(HAVE_INTEL_AVX1) || defined(HAVE_INTEL_AVX2))
 
 static void wb_intel_dispatch(void)
--- tests/unit-mcdc/test_sha512_whitebox.c
+++ tests/unit-mcdc/test_sha512_whitebox.c
@@ -78,7 +78,8 @@ static int wb_fail = 0;
 
 #if !defined(NO_SHA512) && defined(WOLFSSL_SHA512) && \
     defined(WOLFSSL_X86_64_BUILD) && defined(USE_INTEL_SPEEDUP) && \
-    (!defined(WC_C_DYNAMIC_FALLBACK) || defined(HAVE_FIPS)) && \
+    !(defined(WC_C_DYNAMIC_FALLBACK) && \
+      defined(WC_ALLOW_RUNTIME_IMPL_SELECT)) && \
     (defined(HAVE_INTEL_AVX1) || defined(HAVE_INTEL_AVX2))
 
 static void wb_intel_dispatch(void)
