1e8d00bf48ee851ac23103a06b3a7fbe2771a543
Daniele Lacamera <root@danielinux.net>
2026-08-20 20:47:03 +0200

    tests: add three tls extension test files to the tls group
    Empty skeletons registered with automake, cmake and the api test table so
    cases can be added without further build-system changes.

---
+++ tests/api/test_tls_bounds.h
@@ -0,0 +1,30 @@
+/* test_tls_bounds.h
+ *
+ * Copyright (C) 2006-2026 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+#ifndef TESTS_API_TEST_TLS_BOUNDS_H
+#define TESTS_API_TEST_TLS_BOUNDS_H
+
+int test_tls_bounds_placeholder(void);
+
+#define TEST_TLS_BOUNDS_DECLS                                                \
+        TEST_DECL_GROUP("tls", test_tls_bounds_placeholder)
+
+#endif /* TESTS_API_TEST_TLS_BOUNDS_H */
