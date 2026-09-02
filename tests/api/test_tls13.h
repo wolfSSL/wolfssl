517c45f2ede9a6508ffac9d026368bfe8b6984c0
David Garske <david@wolfssl.com>
2026-09-02 14:33:12 -0700

    Add TLS 1.3 crypto callback pend tests and byte-stream test memio

--- tests/api/test_tls13.h
+++ tests/api/test_tls13.h
@@ -132,6 +132,7 @@ int test_tls13_pqc_hybrid_async_server(void);
 int test_tls13_pha_status_request(void);
 int test_tls13_x25519_keyshare_masks_reserved_bit(void);
 int test_tls13_is_init_finished_want_write(void);
+int test_tls13_cryptocb_async(void);
 
 #define TEST_TLS13_DECLS                                        \
     TEST_DECL_GROUP("tls13", test_tls13_apis),                  \
@@ -241,6 +242,7 @@ int test_tls13_is_init_finished_want_write(void);
     TEST_DECL_GROUP("tls13", test_tls13_pqc_hybrid_async_server), \
     TEST_DECL_GROUP("tls13", test_tls13_pha_status_request), \
     TEST_DECL_GROUP("tls13", test_tls13_x25519_keyshare_masks_reserved_bit), \
-    TEST_DECL_GROUP("tls13", test_tls13_is_init_finished_want_write)
+    TEST_DECL_GROUP("tls13", test_tls13_is_init_finished_want_write), \
+    TEST_DECL_GROUP("tls13", test_tls13_cryptocb_async)
 
 #endif /* WOLFCRYPT_TEST_TLS13_H */
