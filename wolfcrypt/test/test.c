6fd33dee47896d520d28bca6754dfaccb92cecb1
Paul Adelsbach <paul.adelsbach@wolfssl.com>
2026-09-25 13:16:37 -0700

    Testing: fix DRBG test failure with wolfHSM

--- wolfcrypt/test/test.c
+++ wolfcrypt/test/test.c
@@ -30257,6 +30257,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t rng_drbg_svc_test(void)
     wc_test_ret_t ret = 0;
     int api_ret;
     int present;
+    int local_gen;
     int root_inited = 0;
     WC_DECLARE_VAR(root, WC_RNG, 1, HEAP_HINT);
     wc_drbg_reseed_ctr_t c1 = 0;
@@ -30306,8 +30307,20 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t rng_drbg_svc_test(void)
 
     present = wc_RNG_DRBG_Present(root);
 
+    /* Set flag to ensure the local DRBG counters are updated in the case
+     * where crypto callbacks are enabled. */
+    local_gen = present;
+#ifdef WOLF_CRYPTO_CB
+    #ifndef WOLF_CRYPTO_CB_FIND
+    if (root->devId != INVALID_DEVID)
+    #endif
+    {
+        local_gen = 0;
+    }
+#endif
+
     /* generate advances the reseed counter */
-    if (present) {
+    if (local_gen) {
         api_ret = wc_RNG_DRBG_GetReseedCtr(root, &c1);
         if ((api_ret != 0) || (c1 < 1))
             ERROR_OUT(WC_TEST_RET_ENC_NC, out);
@@ -30357,7 +30370,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t rng_drbg_svc_test(void)
     api_ret = wc_RNG_GenerateBlock(root, buf, sizeof(buf));
     if (api_ret != 0)
         ERROR_OUT(WC_TEST_RET_ENC_EC(api_ret), out);
-    if (present) {
+    if (local_gen) {
         api_ret = wc_RNG_DRBG_GetReseedCtr(root, &c1);
         if ((api_ret != 0) || (c1 > 2))
             ERROR_OUT(WC_TEST_RET_ENC_NC, out);
